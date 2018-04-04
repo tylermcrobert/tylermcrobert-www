e3287dd6b5337b56df4274044832087fa916ef70
Tyler <tyler.mcrobert@gmail.com>
2018-04-03 21:53:04 -0700

    Nice

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ module.exports = (() => {
   const app = express();
 
   // all environments
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 6969);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
   app.use(favicon('public/images/punch.png'));
