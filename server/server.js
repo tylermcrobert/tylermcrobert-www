f57a217b4bb310c49a8ad0253bea372dacfc3917
Tyler <tyler.mcrobert@gmail.com>
2018-04-08 21:40:37 -0700

    Most recent updates
    Restructured swerver, added favicon, included templates

--- server/server.js
+++ server/server.js
@@ -1,36 +1,14 @@
-const path = require('path');
 const Prismic = require('prismic-javascript');
 const PrismicDOM = require('prismic-dom');
-const PrismicConfig = require('./prismic-configuration');
 const request = require('request');
 const Cookies = require('cookies');
-const express = require('express');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const errorHandler = require('errorhandler');
-
-const app = express();
-
-// all environments
-app.set('port', process.env.PORT || 6969);
-app.set('views', path.join(__dirname, '../views/includes'));
-
-// use pug template
-app.set('view engine', 'pug');
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride());
-
-// directory of templates
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(errorHandler());
-
+const PrismicConfig = require('./prismic-configuration');
+const app = require('./config');
 
 const PORT = app.get('port');
-
+app.listen(PORT, () => {
+  process.stdout.write(`Point your browser to: http://localhost:${PORT}\n`);
+});
 
 // Middleware to inject prismic context
 app.use((req, res, next) => {
@@ -51,24 +29,29 @@ app.use((req, res, next) => {
   });
 });
 
-app.get('/quickstart', (req, res) => {
-  const page = req.params.id;
-  req.prismic.api.getByUID('page', 'quickstart').then((document) => {
-    res.render('test', { document });
+//
+// app.get('/quickstart', (req, res) => {
+//   const page = req.params.id;
+//   req.prismic.api.getByUID('page', 'quickstart').then((document) => {
+//     res.render('test', { document });
+//   });
+// });
+
+app.get('/:caseStudy', (req, res) => {
+  const caseStudy = req.params.caseStudy;
+  req.prismic.api.getByUID('case_study', caseStudy).then((document) => {
+    res.render('casestudy', { document });
   });
 });
 
-app.get('/tester', (req, res) => {
-  res.send(app);
-});
-
-
 app.get('/', (req, res) => {
-  res.render('home');
+  req.prismic.api.getSingle('homepage').then((document) => {
+    res.render('home', { document });
+    // console.log(document.data.case_study_list);
+  });
 });
+
+//eventually redirect this
 app.get('*', (req, res) => {
   res.render('404');
 });
-
-console.log(`point your browser to http://localhost:${PORT}`);
-app.listen(PORT);
