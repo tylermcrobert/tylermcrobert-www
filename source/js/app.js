6d65f154b209fe5be3bef7b3b4b828bb775ebd93
Tyler <tyler.mcrobert@gmail.com>
2018-04-28 00:59:31 -0700

    Started Now Playing

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,17 @@
 import '../sass/app.scss';
+
+import util from './core/util';
+import loadSpotify from './core/spotify';
+
 import home from './views/home';
 import caseStudy from './views/casestudy';
+
 
 const app = {
 
   init() {
     this.loadViews();
+    loadSpotify.init();
   },
 
   loadViews() {
---
+++ source/js/core/spotify.js
@@ -0,0 +1,60 @@
+import util from './util';
+
+
+const loadSpotify = {
+
+  enabled: false,
+
+  init() {
+    this.cacheDom();
+    this.getData();
+    this.bindEvents();
+  },
+
+  cacheDom() {
+    this.dom = {};
+    this.dom.root = document.querySelector('.nowPlaying');
+    this.dom.icon = this.dom.root.querySelector('.nowPlaying__icon');
+    this.dom.content = this.dom.root.querySelector('.nowPlaying__content');
+    this.dom.value = this.dom.root.querySelector('.nowPlaying__content--value');
+  },
+
+  getData(callback) {
+    const base = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks';
+    const user = 'tyler-mcrobert';
+    const apiKey = process.env.LAST_FM_KEY;
+    const url = `${base}&limit=1&user=${user}&api_key=${apiKey}&format=json`;
+
+    util.ajax(url, (response) => {
+      this.response = JSON.parse(response);
+      const track = this.response.recenttracks.track[0];
+
+      this.render(track);
+    });
+  },
+
+  toggleModule() {
+    this.enabled = !this.enabled;
+    this.render();
+  },
+
+  bindEvents() {
+    this.dom.icon.addEventListener('click', () => {
+      this.toggleModule();
+    });
+  },
+
+  render(track) {
+    if (track) {
+      this.dom.value.innerHTML = `${track.name} - ${track.artist['#text']}`;
+    }
+
+    if (!this.enabled) {
+      this.dom.content.classList.remove('-enabled');
+    } else {
+      this.dom.content.classList.add('-enabled');
+    }
+  },
+};
+
+export default loadSpotify;
