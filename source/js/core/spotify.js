1c6350f32728321ee45d611fbd4ef458848d3723
Tyler <tyler.mcrobert@gmail.com>
2018-05-06 23:38:24 -0700

    Fixed non-emoji tracks throwing error

--- source/js/core/spotify.js
+++ source/js/core/spotify.js
@@ -61,9 +61,8 @@ const loadSpotify = {
     this.getAlbumData = (artist) => {
       let albumData = null;
 
-      const albums = artist.albums;
-      if (albums) {
-        albums.forEach((album) => {
+      if (artist && artist.albums) {
+        artist.albums.forEach((album) => {
           if (loadSpotify.currentTrack.album.startsWith(album.title)) {
             albumData = album;
           }
