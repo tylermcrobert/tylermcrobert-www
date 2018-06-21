50fc6981b2c2f7ef9bc3242b457ce88f6d88d4eb
tylermcrobert <31973492+tylermcrobert@users.noreply.github.com>
2018-06-21 09:56:22 -0700

    Conditional tag highlighting

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -2,44 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Tags = (props) => {
-  const tagList = props.tags.map((tag) => {
-    const caseStudyIsSelected = props.currentCaseStudy !== null;
-    const caseStudyIsHovered = !caseStudyIsSelected && props.hoveredCaseStudy;
-    let recievedTag = false;
-
-    if (caseStudyIsSelected) {
-      recievedTag = props.currentCaseStudy.tags.indexOf(tag) !== -1;
-    }
-
-    if (caseStudyIsHovered) {
-      recievedTag = props.hoveredCaseStudy.tags.indexOf(tag) !== -1;
-    }
-
-    const classList = recievedTag ? ['active'] : [null];
-
-    return (
-      <li key={tag} className={`caseStudies__tags ${classList.join('  ')}`}>{tag}</li>
-    );
-  });
-
-  const TagWrapper = styled.div`
+  const TagWrapper = styled.ul`
     display: inline-block;
     transform: rotate(270deg);
     position: absolute;
     left: calc(-50vh + 1em);
     top: 50%;
     width: 100vh;
-    text-align:center;
-
-    li {
-      display: inline-block;
-      margin auto 10px;
-    };
+    text-align: center;
   `;
+
+  const tagList = props.tags.map((tag) => {
+    const csIsSelected = props.currentCaseStudy !== null;
+    const csIsHovered = !csIsSelected && props.hoveredCaseStudy;
+    let recievedTag = false;
+
+    if (csIsSelected) {
+      recievedTag = props.currentCaseStudy.tags.indexOf(tag) !== -1;
+    } else if (csIsHovered) {
+      recievedTag = props.hoveredCaseStudy.tags.indexOf(tag) !== -1;
+    }
+
+    const Tag = styled.li`
+      display: inline-block;
+      margin: auto 10px;
+      color: ${recievedTag ? 'black' : '#aaa'}
+    `;
+
+    return (
+      <Tag key={tag}>{tag}</Tag>
+    );
+  });
 
   return (
     <TagWrapper>
-      <ul className="caseStudies__tags">{tagList}</ul>
+      {tagList}
     </TagWrapper>
   );
 };
