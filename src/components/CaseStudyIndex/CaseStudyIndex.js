4ba2746e72e6f36eab1131307884537831822012
Tyler <tyler.mcrobert@gmail.com>
2018-07-03 00:10:23 -0700

    Staggered index items

--- src/components/CaseStudyIndex/CaseStudyIndex.js
+++ src/components/CaseStudyIndex/CaseStudyIndex.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import { RichText } from 'prismic-reactjs';
+import { Trail, animated } from 'react-spring';
+import { TimingAnimation, Easing } from 'react-spring/dist/addons.cjs';
 import { Link } from 'react-router-dom';
 import CaseStudyContextHOC from '../../containers/CaseStudyContextHOC/CaseStudyContextHOC';
 import './CaseStudyIndex.css';
 
 const CaseStudyIndex = (props) => {
-  const csListData = props.caseStudiesList;
-  const list = csListData.map((caseStudies) => {
+  const list = props.caseStudiesList.map((caseStudies) => {
     const { uid, data: { title } } = caseStudies.case_study_item;
 
     const CaseStudyTitle = styled.h2`
@@ -24,19 +25,42 @@ const CaseStudyIndex = (props) => {
     `;
 
     return (
-      <Link to={uid} key={uid}>
-        <li key={uid} onMouseEnter={() => props.handleHoveredCaseStudy(uid)}>
+      <li
+        className="caseStudyIndex__list__item"
+        onMouseEnter={() => props.handleHoveredCaseStudy(uid)}
+      >
+        <Link to={uid} key={uid}>
           <CaseStudyTitle slug={uid}>
             {RichText.asText(title)}
           </CaseStudyTitle>
-        </li>
-      </Link>
+        </Link>
+      </li>
     );
   });
   return (
     <div className="caseStudyIndex">
       <ul className="caseStudyIndex__list">
-        {list}
+        <Trail
+          native
+          impl={TimingAnimation}
+          config={{ duration: 1200, easing: Easing.bezier(0, 0.75, 0.25, 1) }}
+          from={{ opacity: 0, x: 0.9 }}
+          to={{ opacity: 1, x: 1 }}
+          keys={list}
+        >
+          {list.map(item => ({ x, opacity }) => (
+            <animated.div
+              className="box"
+              onClick={this.toggle}
+              style={{
+                opacity,
+                transform: x.interpolate(x => `scale(${x})`),
+              }}
+            >
+              {item}
+            </animated.div>
+          ))}
+        </Trail>
       </ul>
     </div>
   );
