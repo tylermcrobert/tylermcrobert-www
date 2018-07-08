c516651b659e3498d465d9a00de10ef4b4c97e89
Tyler <tyler.mcrobert@gmail.com>
2018-07-07 22:49:12 -0700

    Fade in case study blocks

--- src/components/CaseStudyBody/CaseStudyBody.js
+++ src/components/CaseStudyBody/CaseStudyBody.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
 import SingleImage from './CaseStudyBlocks/SingleImage/SingleImage';
 import DoubleImage from './CaseStudyBlocks/DoubleImage/DoubleImage';
 import TripleImage from './CaseStudyBlocks/TripleImage/TripleImage';
 import Website from './CaseStudyBlocks/Website/Website';
+import './CaseStudyBlocks.css';
 
 const CaseStudyBody = (props) => {
   const content = props.blocks.map((block, i) => {
     const key = block.slice_type + i;
-
     switch (block.slice_type) {
       case 'single_image':
         return <SingleImage key={key} data={block} />;
@@ -22,7 +23,16 @@ const CaseStudyBody = (props) => {
     }
     return null;
   });
-  return content;
+
+  return content.map(item =>
+    (
+      <ScrollAnimation
+        key={item.key}
+        animateIn="fadeIn"
+        offset={50}
+      >{item}
+      </ScrollAnimation>
+    ));
 };
 
 export default CaseStudyBody;
