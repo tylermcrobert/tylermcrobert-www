824463097fd2a9e904a45ee396b68bfccc3f5278
Tyler <tyler.mcrobert@gmail.com>
2018-12-26 14:01:53 -0500

    CaseStudy improvements

--- src/containers/CaseStudy/CaseStudy.js
+++ src/containers/CaseStudy/CaseStudy.js
@@ -11,17 +11,42 @@ const CaseStudy = ({ data }) => (
         <TitleWrapper>
           <h1>{RichText.asText(data.data.title)}</h1>
         </TitleWrapper>
-        <p>{RichText.asText(data.data.description)}</p>
+        <Info>
+          <Deliverables>
+            {data.data.deliverables}
+          </Deliverables>
+          <Description>
+            {RichText.asText(data.data.description)}
+          </Description>
+        </Info>
       </Cover>
     </ContextFrame>
     <Slices modules={data.data.cs_content} title={RichText.asText(data.data.title)} />
   </div>
 );
 
+const InfoItem = styled.p`
+  margin: 0px 1em;
+`;
 
+const Deliverables = styled(InfoItem)`
+  flex-basis: 30%
+  font-size: .81em;
+`;
+
+const Description = styled(InfoItem)`
+  flex-basis: 70%;
+`;
+
+const Info = styled.div`
+  display: flex;
+  max-width: 48em;
+  flex: 0 1 0%;
+  margin: 0px 0px 2.61792em;
+`;
 const Cover = styled.div`
   /* border: 1px solid blue; */
-  padding: 2em 0; 
+  padding: 2em 0;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
