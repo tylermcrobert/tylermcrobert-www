ae324a9f8f2bba7215dc3add831328a0f9bc3ac9
Tyler <tyler.mcrobert@gmail.com>
2018-12-25 22:28:43 -0500

    Implimented now playing animation

--- src/containers/Nav/NowPlayingIcon/NowPlayingIcon.js
+++ src/containers/Nav/NowPlayingIcon/NowPlayingIcon.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import posed from 'react-pose';
 import NowPlaying from 'containers/NowPlaying/NowPlaying';
+import style from './NowPlayingIcon.module.css';
 
-const NowPlayingIcon = () => (
-  <div>
-    <NowPlaying render={
-      ({
- loaded, song, artist, emoji,
+export default class NowPlayingIcon extends React.Component {
+  state = {
+    drawerOpened: false,
+  }
+
+  toggleDrawer = () => {
+    this.setState({ drawerOpened: !this.state.drawerOpened });
+  }
+  render() {
+    const { drawerOpened } = this.state;
+    return (
+      <NowPlaying render={props => (
+        <Drawer drawerOpened={drawerOpened} toggleDrawer={this.toggleDrawer} {...props} />
+        )}
+      />
+    );
+  }
+}
+
+const Drawer = ({
+  loaded, artist, song, emoji, drawerOpened, toggleDrawer,
 }) => {
-        if (loaded) {
-          return `${emoji} ${artist} — ${song}`;
-        }
-        return null;
-      }}
-    />
-  </div>
-);
+  if (loaded) {
+    return (
+      <SongInfo onClick={toggleDrawer} className={style.nowPlaying} pose={drawerOpened ? 'open' : 'close'}>
+        <div className={style.emoji}>{emoji}</div>
+        <div className={style.songInfo}>
+          {artist} — {song}
+        </div>
+      </SongInfo>
+    );
+  }
+  return null;
+};
 
-export default React.memo(NowPlayingIcon);
+const SongInfo = posed.div({
+  open: { x: '0%' },
+  close: { x: '100%' },
+});
