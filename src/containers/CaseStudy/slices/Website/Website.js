742454661906a0131c22242527875829cb160ddf
Tyler <tyler.mcrobert@gmail.com>
2019-01-27 16:04:11 -0800

    styled components macro

--- src/containers/CaseStudy/slices/Website/Website.js
+++ src/containers/CaseStudy/slices/Website/Website.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 import RoundCorner from 'components/RoundCorner/RoundCorner';
 import Frame from './_Frame';
 
