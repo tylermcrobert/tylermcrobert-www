c3107aa1df663f283975e10a34854b8fc70e13f2
Tyler <tyler.mcrobert@gmail.com>
2019-01-27 16:13:37 -0800

    Added more styled components

--- src/components/ContextFrame/ContextFrame.js
+++ src/components/ContextFrame/ContextFrame.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import style from './ContextFrame.module.css';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
 
 const ContextFrame = ({ children }) => (
-  <div className={style.frame}><div>{children}</div></div>
+  <Styled.Frame>
+    {children}
+  </Styled.Frame>
 );
 
+ContextFrame.propTypes = {
+  children: PropTypes.func.isRequired,
+};
+
 export default ContextFrame;
+
+const Styled = {};
+
+Styled.Frame = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 3rem;
+`;
--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components/macro';
+
 import NowPlayingIcon from './NowPlayingIcon/NowPlayingIcon';
-import style from './Nav.module.css';
 
 const Nav = () => (
-  <div className={style.nav}>
-    <Link className={style.logo} to="/">Tyler McRobert</Link>
+  <Styled.Nav>
+    <Styled.Logo to="/">Tyler McRobert</Styled.Logo>
     <div>info</div>
     <NowPlayingIcon />
-  </div>
+  </Styled.Nav>
 );
+
+const Styled = {};
+
+Styled.Nav = styled.nav`
+  position: fixed;
+  display: flex;
+  width: 100%;
+  position: fixed;
+  z-index: 100;
+  padding: .25em .5em;
+
+  * {
+    margin: .25em .5em;
+  }
+
+`;
+
+Styled.Logo = styled(Link)`
+  flex: 1;
+`;
 
 export default Nav;
