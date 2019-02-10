7bdd4d1c6d891f02b19275032ec3c16720651adc
Tyler <tyler.mcrobert@gmail.com>
2019-02-09 17:54:58 -0800

    Reset csContext after return to homepage

--- src/containers/Context/hooks/useCsContext.js
+++ src/containers/Context/hooks/useCsContext.js
@@ -1,16 +1,35 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect, useRef } from 'react';
 import { AppContext } from 'containers/App/App';
+
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 
 export default function useCsContext() {
   const [hoverTags, setHoverTags] = useState([]);
   const [hoverIndex, setHoverIndex] = useState(0);
 
-  const { index } = useContext(AppContext);
+  const { index, view } = useContext(AppContext);
+  const previousView = usePrevious(view);
 
   const handleHover = (hoveredTag, i) => {
     setHoverTags(hoveredTag);
     setHoverIndex(i);
   };
+
+  useEffect(() => {
+    if (previousView !== 'home') {
+      setHoverIndex(0);
+      setHoverTags([]);
+    }
+  }, [view]);
+
 
   return {
     tags: hoverTags,
