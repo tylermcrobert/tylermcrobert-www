9eee84ccb54db57488b097fc250af514caadfca4
Tyler <tyler.mcrobert@gmail.com>
2019-02-20 18:28:07 -0800

    Intersection observer polyfill

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import Router from 'containers/Router/Router';
 import ReactDOM from 'react-dom';
 
+require('intersection-observer');
+
 ReactDOM.render(<Router />, document.getElementById('root'));
