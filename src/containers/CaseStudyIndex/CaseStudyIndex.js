9f6cd256edde41baf4532dfaa7bf94166b8d66b4
Tyler <tyler.mcrobert@gmail.com>
2019-02-23 16:02:53 -0800

    Removed scrolltop

--- src/containers/CaseStudyIndex/CaseStudyIndex.js
+++ src/containers/CaseStudyIndex/CaseStudyIndex.js
@@ -8,11 +8,11 @@ import Styled, { ZoomIn } from './blocks';
 const DirectoryLinks = ({ caseStudies, location }) => {
   const handleHover = useContext(ContextFrameContext);
 
-  useEffect(() => {
-    setTimeout(() => {
-      window.scrollTo(0, 0);
-    }, 400);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     window.scrollTo(0, 0);
+  //   }, 400);
+  // }, []);
 
   return (
     <FullFrame context>
