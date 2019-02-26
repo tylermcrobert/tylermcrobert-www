34ec7e16d164f414a18796ea6a3380cdde0d8505
Tyler <tyler.mcrobert@gmail.com>
2019-02-25 20:12:43 -0800

    Cleaned up Info

--- src/containers/Info/Info.js
+++ src/containers/Info/Info.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import styled from 'styled-components/macro';
-import posed from 'react-pose';
-import { poses } from 'containers/App/styled';
+import Styled from './styled';
 
 function Info() {
   return (
@@ -12,28 +10,6 @@ function Info() {
     </Styled.PageContainer>
   );
 }
-
-const Styled = {};
-
-const TitleIn = posed.div(poses.fadeUp);
-
-
-Styled.PageContainer = styled(TitleIn)`
-  padding: 1rem;
-`;
-
-const fontSize = 'calc(.75em + 2vw)';
-
-Styled.Heading = styled.div`
-  min-height: 66vh;
-  font-size: ${fontSize};
-  line-height: 1.4;
-  padding: ${fontSize};
-  text-indent: calc((.375em + 1vw) * -1);
-
-  display: flex;
-  align-items: center;
-`;
 
 
 export default Info;
