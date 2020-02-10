ec4d634021b99e5c1a9631b2df969fecc60cdda3
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-10 04:50:32 -0800

    Clear project

--- src/components/Page/Page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-import Styled from './styled';
-
-export default function Page({ children }) {
-  return <Styled.PageContainer>{children}</Styled.PageContainer>;
-}
--- src/containers/App/styled.js
+++
@@ -1,8 +0,0 @@
-import { sizes, media } from './styled/media';
-import GlobalStyle from './styled/GlobalStyle';
-import { durations, eases, transitions, poses } from './styled/anim';
-import theme from './styled/theme';
-
-export { theme, sizes, media, durations, eases, transitions, poses };
-
-export default GlobalStyle;
