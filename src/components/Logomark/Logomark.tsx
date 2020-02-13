2e12287553b65141ca59814b8128ecfeab284927
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-12 22:30:38 -0800

    props spread on logomark

--- src/components/Logomark/Logomark.tsx
+++ src/components/Logomark/Logomark.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 
-const Logomark = () => {
+const Logomark = ({ ...props }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 224 226">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 224 226" {...props}>
       <path
         style={{ width: "100%" }}
         d="M62 26l100 174M25 163L199 63M62 200L162 26"
