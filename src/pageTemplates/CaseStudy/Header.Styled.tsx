e74abe4e3a7b9f24f84408cd99ffbb957cb8dbe1
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-16 20:54:40 -0800

    gap

--- src/pageTemplates/CaseStudy/Header.Styled.tsx
+++ src/pageTemplates/CaseStudy/Header.Styled.tsx
@@ -10,7 +10,7 @@ const Col = styled.div`
 `
 
 const Title = styled(Col)`
-  margin-top: ${props => props.theme.remScale[7]};
+  margin-top: ${props => props.theme.remScale[6]};
 `
 
 const Intro = styled(Col)`
