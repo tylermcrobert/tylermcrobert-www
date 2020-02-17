dad5860587bf4399c9cfeeafd9e54d27372da973
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-16 20:55:46 -0800

    wrapper width

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -3,6 +3,6 @@ import styled from "styled-components"
 const Wrapper = styled.div`
   padding: 0 ${props => props.theme.margins.standard};
   margin: 0 auto;
-  max-width: 60rem;
+  max-width: 70rem;
 `
 export default Wrapper
