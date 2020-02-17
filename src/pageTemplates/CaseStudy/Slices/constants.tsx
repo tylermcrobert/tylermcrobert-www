3bf79cf24800140ee3ea95a4489ab680e888fa7b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-17 11:09:29 -0800

    use jpg

--- src/pageTemplates/CaseStudy/Slices/constants.tsx
+++ src/pageTemplates/CaseStudy/Slices/constants.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 import { FmType } from "util/formatImg"
 
-export const HALF_SIZE = { w: 1200, fm: "webp" as FmType }
-export const FULL_SIZE = { w: 1920, fm: "webp" as FmType }
+export const HALF_SIZE = { w: 1200, fm: "jpg" as FmType }
+export const FULL_SIZE = { w: 1920, fm: "jpg" as FmType }
