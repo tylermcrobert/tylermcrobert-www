3494230f4fd88eff6eb483aa734e6ad237be6e67
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-17 10:42:13 -0800

    remove unneded query from homepage

--- src/templates/homepage.tsx
+++ src/templates/homepage.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { graphql } from "gatsby"
 import { Layout, Seo } from "components"
 import { parseSearch } from "components/ClientContextProvider"
 import { Homepage } from "../pageTemplates"
@@ -7,84 +6,14 @@ import { Homepage } from "../pageTemplates"
 interface IProps {
   pageContext: { uid: string }
   location: { search: string }
-  data: IPrismicContext
 }
-const HomepageContainer: React.FC<IProps> = ({
-  pageContext,
-  data,
-  location,
-}) => {
+const HomepageContainer: React.FC<IProps> = ({ pageContext, location }) => {
   return (
     <Layout ctx={pageContext.uid || parseSearch(location.search)}>
       <Seo title={null} />
-      <Homepage data={data} />
+      <Homepage />
     </Layout>
   )
 }
-
-export interface Meta {
-  uid: string
-}
-
-export interface Title {
-  type: string
-  text: string
-  spans: any[]
-}
-
-export interface Meta2 {
-  uid: string
-}
-
-export interface CaseStudyItem {
-  __typename: string
-  title: Title[]
-  deliverables: string
-  _meta: Meta2
-}
-
-export interface CaseStudyList {
-  case_study_item: CaseStudyItem
-}
-
-export interface Context {
-  _meta: Meta
-  case_study_list: CaseStudyList[]
-}
-
-export interface Prismic {
-  context: Context
-}
-
-export interface IPrismicContext {
-  prismic: Prismic
-}
-
-export interface IPrismicHomepageQuery {
-  prismic: Prismic
-}
-
-export const query = graphql`
-  query HomepageQuery($uid: String!) {
-    prismic {
-      context(lang: "en-us", uid: $uid) {
-        _meta {
-          uid
-        }
-        case_study_list {
-          case_study_item {
-            ... on PRISMIC_Case_study {
-              title
-              deliverables
-              _meta {
-                uid
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
 
 export default HomepageContainer
