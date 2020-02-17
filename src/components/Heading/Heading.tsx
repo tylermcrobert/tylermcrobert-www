09cebea2897934f3a8d8ae62fcaf125c41bf800b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-17 10:39:52 -0800

    adjust heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,7 +1,20 @@
 import styled from "styled-components"
+import { mq } from "style"
 
 export const LargeHead = styled.h1`
-  font-size: 3.1rem;
+  font-size: ${props => props.theme.remScale[3]};
+
+  @media ${mq.xs} {
+    font-size: ${props => props.theme.remScale[4]};
+  }
+
+  @media ${mq.sm} {
+    font-size: ${props => props.theme.remScale[5]};
+  }
+
+  @media ${mq.md} {
+    font-size: ${props => props.theme.remScale[6]};
+  }
 `
 
 export const MediumHead = styled.h2`
