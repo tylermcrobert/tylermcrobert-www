b0eb6c060bd0cadbdd272374c5bac399fef70b99
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-17 17:29:45 -0800

    update checkMobile

--- src/util/checkMobile.tsx
+++ src/util/checkMobile.tsx
@@ -1,6 +1,10 @@
-const checkMobile = () =>
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
+const checkMobile = (): boolean => {
+  if (typeof window !== "undefined") {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  }
+  return false
+}
 
 export default checkMobile
