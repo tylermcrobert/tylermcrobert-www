20aa44b429904a7804780a1e4427049140a9d94e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-18 22:35:57 -0800

    Jesus christ!

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,4 +1,3 @@
 import { useCuration } from "components/CurationProvider"
-import { useCaseStudyCtx } from "pageTemplates/CaseStudy/CaseStudy"
 
-export { useCuration, useCaseStudyCtx }
+export { useCuration }
