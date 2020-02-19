49372221bd3898d0e826d2463ebbaff9d21a933e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-18 22:47:25 -0800

    Update browser frame

--- src/pageTemplates/CaseStudy/Slices/SliceWebsite.tsx
+++ src/pageTemplates/CaseStudy/Slices/SliceWebsite.tsx
@@ -45,11 +45,15 @@ const BrowserFrame: React.FC<{ color?: string }> = ({ color = "#D8D8D8" }) => (
     viewBox="0 0 439 13"
     preserveAspectRatio="none"
   >
-    <path
-      d="M435 0c2.209139 0 4 1.790861 4 4v9H0V4c0-2.209139 1.790861-4 4-4h431zM6.5 4C5.11928813 4 4 5.11928813 4 6.5S5.11928813 9 6.5 9 9 7.88071187 9 6.5 7.88071187 4 6.5 4zm16 0C21.1192881 4 20 5.11928813 20 6.5S21.1192881 9 22.5 9 25 7.88071187 25 6.5 23.8807119 4 22.5 4zm-8 0C13.1192881 4 12 5.11928813 12 6.5S13.1192881 9 14.5 9 17 7.88071187 17 6.5 15.8807119 4 14.5 4z"
-      fill={color}
-      fillRule="evenodd"
-    />
+    <g fill="none" fillRule="evenodd">
+      <path
+        d="M4 0h431c2.209139 0 4 1.790861 4 4v9H0V4c0-2.209139 1.790861-4 4-4z"
+        fill={color}
+      />
+      <circle fill="#FF6158" cx="6.5" cy="6.5" r="2.5" />
+      <circle fill="#FFBE2D" cx="22.5" cy="6.5" r="2.5" />
+      <circle fill="#27C93F" cx="14.5" cy="6.5" r="2.5" />
+    </g>
   </svg>
 )
 export default Website
