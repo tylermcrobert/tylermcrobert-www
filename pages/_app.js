5cecd1eabf9cb66bec1a05f53eae95790f2e1cbd
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-27 17:50:06 -0800

    Update app

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,15 @@
-import Prismic from "prismic-javascript";
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export const apiEndpoint = "https://tylermcrobert.cdn.prismic.io/api/v2";
-export const accessToken = "";
+// MyApp.getInitialProps = async ({ appContext, ctx }) => {
+//   const ctxData = await Client(ctx.req).query(
+//     Prismic.Predicates.at("document.type", "context")
+//   );
 
-// Client method to query documents from the Prismic repo
-export const Client = (req = null) =>
-  Prismic.client(apiEndpoint, createClientOptions(req, accessToken));
+//   const data = parseCuration(ctxData);
 
-const createClientOptions = (req = null, prismicAccessToken = null) => {
-  const reqOption = req ? { req } : {};
-  const accessTokenOption = prismicAccessToken
-    ? { accessToken: prismicAccessToken }
-    : {};
-  return {
-    ...reqOption,
-    ...accessTokenOption
-  };
-};
-
-MyApp.getInitialProps = async ({ appContext, ctx }) => {
-  const ctxData = await Client(ctx.req).query(
-    Prismic.Predicates.at("document.type", "context")
-  );
-
-  console.log(ctxData.results.map(res => res.data));
-
-  return { ctxData };
-};
+//   return { ctxData };
+// };
 
 export default MyApp;
