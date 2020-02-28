7d4ac16e07c25c66fdca503304d6000a2f4473a3
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-27 21:48:23 -0800

    add grid

---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import Wrapper from "../Wrapper"
+
+export const Grid = styled(Wrapper)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: ${props => props.theme.margins.gutter};
+`
