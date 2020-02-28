886015dfd9b1066e85ad6ffd93d9ccb363d3ee27
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-27 17:55:21 -0800

    update prismic util

--- src/util/prismic.tsx
+++ src/util/prismic.tsx
@@ -1,18 +1,18 @@
-import Prismic from "prismic-javascript";
+import Prismic from "prismic-javascript"
 
-export const apiEndpoint = "https://tylermcrobert.cdn.prismic.io/api/v2";
-export const accessToken = "";
-
-export const Client = (req = null) =>
-  Prismic.client(apiEndpoint, createClientOptions(req, accessToken));
+export const apiEndpoint = "https://tylermcrobert.cdn.prismic.io/api/v2"
+export const accessToken = ""
 
 const createClientOptions = (req = null, prismicAccessToken = null) => {
-  const reqOption = req ? { req } : {};
+  const reqOption = req ? { req } : {}
   const accessTokenOption = prismicAccessToken
     ? { accessToken: prismicAccessToken }
-    : {};
+    : {}
   return {
     ...reqOption,
-    ...accessTokenOption
-  };
-};
+    ...accessTokenOption,
+  }
+}
+
+export const Client = (req = null) =>
+  Prismic.client(apiEndpoint, createClientOptions(req, accessToken))
