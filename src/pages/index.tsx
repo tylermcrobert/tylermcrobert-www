824a40e03495e3a55f3d317143960d76ed5b7457
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-28 13:54:10 -0800

    add basic case study shit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,43 @@
 import Prismic from "prismic-javascript"
 import { NextPage } from "next"
 import { Client } from "util/prismic"
-import { IPrismicCaseStudyRes } from "types/api/PrismicApiCaseStudy"
+import { IPrismicCaseStudyRes, ICaseStudy } from "types/api/PrismicApiCaseStudy"
+import { asText, RichText } from "util/richText"
 
 interface IProps {
   caseStudiesRes: IPrismicCaseStudyRes
 }
 
 const Home: NextPage<IProps> = ({ caseStudiesRes }) => {
-  return <div>asdfasdfs</div>
+  return (
+    <div>
+      {caseStudiesRes.results.map(item => (
+        <CaseStudy data={item} />
+      ))}
+    </div>
+  )
+}
+
+const CaseStudy: React.FC<{ data: ICaseStudy }> = ({ data: { data } }) => {
+  const title = asText(data.title)
+  const { deliverables, intro, description } = data
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>
+        <RichText>{intro}</RichText>
+      </h2>
+      <br />
+
+      <p>{deliverables}</p>
+      <br />
+
+      <RichText>{description}</RichText>
+      <br />
+      <hr />
+    </div>
+  )
 }
 
 Home.getInitialProps = async ctx => {
