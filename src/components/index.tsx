ecc82952eafdd5247dced2c85fc3d721fcf08f66
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-27 21:56:00 -0800

    add components

---
+++ src/components/index.tsx
@@ -0,0 +1,21 @@
+import Paragraph from "./Paragraph"
+import Html from "./Html"
+import { LargeHead, MediumHead } from "./Heading"
+import { Grid } from "./Grid"
+import Wrapper from "./Wrapper"
+import Seo from "./Seo"
+import DotHead from "./DotHead"
+import { Section, SmallSection } from "./Section"
+
+export {
+  MediumHead,
+  LargeHead,
+  Html,
+  Grid,
+  Section,
+  SmallSection,
+  Paragraph,
+  Wrapper,
+  Seo,
+  DotHead,
+}
