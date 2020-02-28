f898019d7587d829f95b48aad4c3db933ee66001
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-27 21:25:56 -0800

    add /style/

---
+++ src/style/index.ts
@@ -0,0 +1,2 @@
+export { default as modularScale } from "./modularScale"
+export { default as mq } from "./mq"
---
+++ src/style/theme.tsx
@@ -0,0 +1,42 @@
+import { DefaultTheme } from "styled-components"
+import modularScale from "./modularScale"
+
+const scale: number[] = modularScale({ scale: 1.333, stepsDown: 2, length: 15 })
+
+const remScale: string[] = scale.map(item => `${item}rem`)
+
+const theme: DefaultTheme = {
+  scale,
+  remScale,
+
+  colors: {
+    primary: "#000000",
+    secondary: "#ffffff",
+  },
+
+  fontFamilies: {
+    sansSerif: `Suisse, helvetica, arial, sans-serif`,
+    serif: `"SouvenirStd-light"`,
+  },
+
+  lineHeights: {
+    body: 1.2,
+    heading: 1.05,
+  },
+
+  margins: {
+    standard: remScale[3],
+    gutter: remScale[3],
+    large: remScale[8],
+  },
+
+  sizes: {
+    xl: 1900,
+    lg: 1440,
+    md: 1024,
+    sm: 768,
+    xs: 576,
+  },
+}
+
+export default theme
