7f9489e5c5c8f56ec131233c5248685ca11ab6a6
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-01 11:22:33 -0800

    remove h1-6 and p from richtext

--- src/util/richText.tsx
+++ src/util/richText.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-case-declarations */
 import React from "react"
-import { RichText as PrismicRichText } from "prismic-reactjs"
+import { RichText as PrismicRichText, Elements } from "prismic-reactjs"
 import { IPrismicRichText } from "types/PrismicGeneric"
 
 export const asText = (text: IPrismicRichText): string =>
@@ -9,6 +10,18 @@ interface IRichTextProps {
   children: IPrismicRichText
 }
 
+const htmlSerializer = (type, element, content, children, key) => {
+  return type === Elements.heading1 ||
+    type === Elements.heading2 ||
+    type === Elements.heading3 ||
+    type === Elements.heading4 ||
+    type === Elements.heading5 ||
+    type === Elements.heading6 ||
+    type === Elements.paragraph
+    ? children
+    : null
+}
+
 export const RichText: React.FC<IRichTextProps> = ({ children }) => (
-  <>{PrismicRichText.render(children)}</>
+  <PrismicRichText render={children} htmlSerializer={htmlSerializer} />
 )
