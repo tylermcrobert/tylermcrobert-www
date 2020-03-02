dea560e6dccebc827788048c8fce1ab016a8187a
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-01 17:43:01 -0800

    getPlaylist works

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -3,6 +3,7 @@ import { Client } from "util/prismic"
 import { NextPage } from "next"
 import { IInfoRes } from "types/Prismic"
 import { Info } from "components"
+import { getAuthToken, getPlaylist } from "util/getPlaylist"
 
 interface IProps {
   infoRes?: IInfoRes
@@ -17,6 +18,12 @@ export async function getStaticProps({ req }) {
     Prismic.Predicates.at("document.type", "info"),
     {}
   )
+
+  const id = "5iibda0SvRRNw0GPyQHx01"
+  const token = await getAuthToken()
+  const playlistData = await getPlaylist({ id, token })
+
+  console.log(playlistData)
 
   return {
     props: { infoRes },
--- src/util/getPlaylist.tsx
+++ src/util/getPlaylist.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const fetch = require("isomorphic-unfetch")
-
 const clientId = process.env.SPOTIFY_CLIENT_ID
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
 
@@ -10,7 +7,7 @@ const clientSecret = process.env.SPOTIFY_CLIENT_SECRET
  * https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
  */
 
-const getAuthToken = async () => {
+export const getAuthToken = async () => {
   const authFormatted = `${clientId}:${clientSecret}`
   const base64Auth = Buffer.from(authFormatted).toString("base64")
 
@@ -30,7 +27,13 @@ const getAuthToken = async () => {
 /**
  * Get Spotify Playlist
  */
-const getPlaylist = async ({ id, token }: { id: string; token: string }) => {
+export const getPlaylist = async ({
+  id,
+  token,
+}: {
+  id: string
+  token: string
+}) => {
   const data = await fetch(`https://api.spotify.com/v1/playlists/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -38,20 +41,4 @@ const getPlaylist = async ({ id, token }: { id: string; token: string }) => {
   }).then(res => res.json())
 
   return data
-}
-
-/**
- * Main
- */
-
-export default async (req, res) => {
-  const { id } = req.query
-
-  if (id) {
-    const token = await getAuthToken()
-    const playlistData = await getPlaylist({ id, token })
-    res.status(200).json(playlistData)
-  } else {
-    res.status(401).json({ error: "ID field is required" })
-  }
 }
