8c2e7c3e584c1c96b9558d424a326da461e748e3
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-03 19:18:08 -0800

    Clean house

--- src/components/CaseStudyPicker/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default } from "./CaseStudyPicker"
-export { default as PickerWrapper } from "./PickerWrapper/PickerWrapper"
--- src/components/Wrapper/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-const Wrapper = styled.div`
-  padding: 0 ${props => props.theme.margins.standard};
-  margin: 0 auto;
-  max-width: 70rem;
-  width: 100%;
-`
-export default Wrapper
