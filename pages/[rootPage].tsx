fadb225c150817b7600c8186a6857faae9731015
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-03-03 20:16:13 -0800

    basic getInitialProps setup

--- pages/[rootPage].tsx
+++ pages/[rootPage].tsx
@@ -1,7 +1,24 @@
 import React from "react"
+import { NextPage } from "next"
+import { Client } from "../util/prismic"
 
-const RootPage = () => {
-  return <div>ROOT PAGE</div>
+const RootPage: NextPage<{ data: any }> = ({ data }) => {
+  // console.log(data)
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+export const getStaticProps = async ctx => {
+  const res = await Client().getByUID("case_study", ctx.params.rootPage, {})
+
+  return { props: { data: res } }
+}
+
+export const getStaticPaths = () => {
+  return {
+    fallback: true,
+    paths: [{ params: { rootPage: "hightidenyc" } }],
+  }
 }
 
 export default RootPage
--- pages/preview.tsx
+++ pages/preview.tsx
@@ -1,32 +1,6 @@
 import React from "react"
-
 import Router from "next/router"
-import Prismic from "prismic-javascript"
-
-export const apiEndpoint = "https://tylermcrobert.cdn.prismic.io/api/v2"
-export const accessToken = ""
-
-const createClientOptions = (req = null, prismicAccessToken = null) => {
-  const reqOption = req ? { req } : {}
-  const accessTokenOption = prismicAccessToken
-    ? { accessToken: prismicAccessToken }
-    : {}
-  return {
-    ...reqOption,
-    ...accessTokenOption,
-  }
-}
-
-export const Client = (req = null) =>
-  Prismic.client(apiEndpoint, createClientOptions(req, accessToken))
-
-export const linkResolver = doc => {
-  if (doc.type === "case_study") return `/${doc.uid}`
-  if (doc.type === "info") return `/info`
-
-  // Fallback for other types, in case new custom types get created
-  return `/${doc.uid}`
-}
+import { Client, linkResolver } from "../util/prismic"
 
 const Preview = () => {
   return <div>Loading preview...</div>
