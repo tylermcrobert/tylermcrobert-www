018efe5f00257d13c2024d50ae6499d0abe5d954
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-12 14:59:37 -0700

    clear project

--- src/components/Paragraph/Paragraph.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-const Paragraph = styled.div`
-  text-indent: 2em;
-`
-
-export default Paragraph
--- src/hooks/useInitial.tsx
+++
@@ -1,8 +0,0 @@
-import { useRef } from "react"
-
-const useInitial = input => {
-  const ref = useRef(input)
-  return ref.current
-}
-
-export default useInitial
