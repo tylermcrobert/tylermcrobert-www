32c313b0d26d806296cac212f610892314f5c809
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-12 15:22:04 -0700

    install sanity client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,16 @@ import Prismic from "prismic-javascript"
 import { Client } from "util/prismic"
 import useInitial from "hooks/useInitial"
 
+import sanityClient from "@sanity/client"
+
+const client = sanityClient({
+  projectId: "n1wxk3oc",
+  dataset: "production",
+  token:
+    "sk1FVAGjsEGz1l0egzr4gY4SD6ZSEQ8wCPqrFJelnZvS27jNryBkavrUyon6iRYEN9IlzypYrLt0Ipqi8EAM3CeEaWIXxW60PvMNR4D5iJMyCd6ECeLKXfG6kGosmWV4I0uqEWbZxSdWA47WooqjjD5PuiLxyFdYMFbVGNSiS3BKYFAF1vBl", // or leave blank to be anonymous user
+  useCdn: true, // `false` if you want to ensure fresh data
+})
+
 export const DataCtx = createContext<{
   caseStudiesRes: IPrismicCaseStudyRes
   ctxRes: IContextRes
@@ -31,6 +41,16 @@ const MyApp = ({
   const ctxData: IContextRes = useInitial(ctxRes)
   const caseStudiesData: IPrismicCaseStudyRes = useInitial(caseStudiesRes)
 
+  console.log(
+    caseStudiesData.results.forEach((result) => {
+      console.log(result)
+      // client.transaction().createOrReplace({
+      //   _id: result.id,
+      //   _type: "caseStudy",
+      // })
+    })
+  )
+
   return (
     <DataCtx.Provider
       value={{
@@ -51,7 +71,7 @@ const MyApp = ({
   )
 }
 
-MyApp.getInitialProps = async appContext => {
+MyApp.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext) // keep this
 
   if (!process.browser) {
