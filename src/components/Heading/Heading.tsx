6b25a0825e4476fdf7eb09a5541f1ff8ed5c206a
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-13 11:25:20 -0700

    update heading size

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { mq, size, lineHeights } from 'style'
 
 export const LargeHead = styled.h1`
-  font-size: ${size[4]};
+  font-size: ${size[2]};
   line-height: ${lineHeights.heading};
 
   br {
