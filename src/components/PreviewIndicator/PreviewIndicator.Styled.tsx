3e4c6f84c0f7ac86eeae69684af7c39defde0d2e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-13 12:33:06 -0700

    draft preview indicator

--- src/components/PreviewIndicator/PreviewIndicator.Styled.tsx
+++ src/components/PreviewIndicator/PreviewIndicator.Styled.tsx
@@ -1,15 +1,32 @@
 import styled from 'styled-components'
 import { size } from 'style'
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ enabled: boolean }>`
   background: orange;
   font-size: ${size[-1]};
-  padding: ${size[-2]};
+  padding: 0 ${size[-2]};
   margin: ${size[-2]};
   position: fixed;
   bottom: 0;
   left: 0;
   text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: ${size[2]};
+  width: ${p => (p.enabled ? `250px` : size[2])};
+  overflow: hidden;
+  white-space: nowrap;
+  border-radius: 999px;
+  transform: scale(${p => (p.enabled ? 1 : 0.5)});
+  transition: 200ms width ease, 200ms transform ease;
+  transform-origin: bottom left;
+
+  > div {
+    opacity: ${p => (p.enabled ? 1 : 0)};
+    transition: 200ms opacity ease;
+  }
 `
 
 export default {
--- src/components/PreviewIndicator/PreviewIndicator.tsx
+++ src/components/PreviewIndicator/PreviewIndicator.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import S from './PreviewIndicator.Styled'
 
 const PreviewIndicator = () => {
+  const [isEnabled, setEnabled] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setEnabled(false)
+    }, 2000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [isEnabled])
+
   return (
-    <S.Wrapper>
-      Draft Preview <a href="/api/clear-preview">[X]</a>
+    <S.Wrapper enabled={isEnabled} onMouseOver={() => setEnabled(true)}>
+      <div>
+        Draft Preview <a href="/api/clear-preview">[X]</a>
+      </div>
     </S.Wrapper>
   )
 }
