ec32517e837d3e940de37444074f1354cbcd2989
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-13 17:47:20 -0700

    prettier on schema

--- cms/schemas/caseStudy.js
+++ cms/schemas/caseStudy.js
@@ -1,79 +1,79 @@
 export default {
-  type: "document",
-  name: "caseStudy",
+  type: 'document',
+  name: 'caseStudy',
   fields: [
     {
-      type: "slug",
-      name: "slug",
+      type: 'slug',
+      name: 'slug',
     },
     {
-      name: "title",
-      type: "string",
+      name: 'title',
+      type: 'string',
     },
     {
-      name: "date",
-      type: "datetime",
-      validation: (Rule) => Rule.required(),
+      name: 'date',
+      type: 'datetime',
+      validation: Rule => Rule.required(),
     },
     {
-      name: "intro",
-      type: "text",
+      name: 'intro',
+      type: 'text',
       rows: 2,
     },
 
     {
-      name: "description",
-      type: "array",
-      of: [{ type: "block" }],
+      name: 'description',
+      type: 'array',
+      of: [{ type: 'block' }],
     },
     {
-      name: "deliverables",
-      type: "array",
-      of: [{ type: "string" }],
+      name: 'deliverables',
+      type: 'array',
+      of: [{ type: 'string' }],
     },
     {
-      name: "modules",
-      type: "array",
+      name: 'modules',
+      type: 'array',
       of: [
         {
-          name: "singleImage",
-          type: "object",
+          name: 'singleImage',
+          type: 'object',
           fields: [
             {
-              name: "image",
-              type: "image",
+              name: 'image',
+              type: 'image',
             },
           ],
         },
         {
-          name: "doubleImage",
-          type: "object",
+          name: 'doubleImage',
+          type: 'object',
           fields: [
             {
-              name: "leftImage",
-              type: "image",
+              name: 'leftImage',
+              type: 'image',
             },
             {
-              name: "rightImage",
-              type: "image",
+              name: 'rightImage',
+              type: 'image',
             },
           ],
         },
         {
-          name: "tripleImage",
-          type: "object",
+          name: 'tripleImage',
+          type: 'object',
           fields: [
             {
-              name: "mainImage",
-              type: "image",
+              name: 'mainImage',
+              type: 'image',
             },
             {
-              name: "secondaryImage1",
-              type: "image",
+              name: 'secondaryImage1',
+              type: 'image',
             },
             {
-              name: "secondaryImage2",
-              type: "image",
+              name: 'secondaryImage2',
+              type: 'image',
             },
           ],
         },
