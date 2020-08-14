ea78c2797b471c4f8a6976ccd65f31df50e5b2e9
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-08-14 10:07:22 -0700

    update cms

--- cms/resolveProductionUrl.js
+++ cms/resolveProductionUrl.js
@@ -2,5 +2,6 @@ const isDev = process.env.NODE_ENV === 'development'
 const projectUrl = isDev ? 'http://localhost:3000' : 'https://tylermcrobert.com'
 
 export default function resolveProductionUrl(document) {
+  if (!document.slug) return null
   return `${projectUrl}/api/preview?slug=${document.slug.current}&type=${document._type}`
 }
