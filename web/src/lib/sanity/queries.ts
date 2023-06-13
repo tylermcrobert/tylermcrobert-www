d4d65da709fb809538a54bfa1e7103f8df29b0c7
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-12 20:58:11 -0700

    Add info page information

--- web/src/lib/sanity/queries.ts
+++ web/src/lib/sanity/queries.ts
@@ -3,9 +3,21 @@ import groq from 'groq';
 export const infoQuery = groq`
   *[_type == 'info' ][0]{
     bio,
+    playlists[]-> {
+      "slug": slug.current,
+      link,
+      title,
+    }
   }
 `;
 
+export type InfoPlaylist = {
+	link: string;
+	title: string;
+	slug: string;
+};
+
 export type InfoQuery = {
 	bio: string;
+	playlists: InfoPlaylist[];
 };
