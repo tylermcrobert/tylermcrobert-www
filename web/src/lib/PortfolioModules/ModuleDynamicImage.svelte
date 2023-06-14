83937c8bd229a284f5d0ad12f985501ec09aad9c
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-13 20:52:30 -0700

    Clean up dynamic image

--- web/src/lib/PortfolioModules/ModuleDynamicImage.svelte
+++ web/src/lib/PortfolioModules/ModuleDynamicImage.svelte
@@ -2,6 +2,22 @@
   import ResponsiveImage from '$lib/ResponsiveImage.svelte';
   import type { ModuleDynamicImage } from '$lib/sanity/queries';
   export let data: ModuleDynamicImage;
+
+  let half = data.span === 'half';
+  let full = data.span === 'full';
 </script>
 
-<ResponsiveImage image={data.image} alt="" sizes="100vw" />
+<div class="img" class:half class:full>
+  <ResponsiveImage
+    image={data.image}
+    alt=""
+    sizes={half ? '50vw' : '100vw'}
+    aspect={data.aspect}
+  />
+</div>
+
+<style>
+  .img :global(img) {
+    max-width: 100%;
+  }
+</style>
