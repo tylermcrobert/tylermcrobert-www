f0852be42cfe98a51c3bdf5f8dc89d101656000e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-16 16:17:02 -0700

    Basic config for previews

---
+++ web/src/routes/api/preview/+server.ts
@@ -0,0 +1,19 @@
+import type { RequestHandler } from './$types';
+import { error, redirect } from '@sveltejs/kit';
+
+export const GET: RequestHandler = ({ url }) => {
+  const allParams = url.searchParams;
+  const type = allParams.get('type');
+  const slug = allParams.get('slug');
+
+  if (!slug || !type) {
+    throw error(401, 'Missing slug or type');
+  }
+
+  if (type === 'caseStudy') {
+    const url = `/${slug}`;
+    throw redirect(302, url);
+  }
+
+  throw redirect(302, '/404');
+};
