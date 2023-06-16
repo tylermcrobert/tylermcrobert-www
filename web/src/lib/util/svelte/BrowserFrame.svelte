b2604688e064c127e3141341500e9ea070d155d6
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-16 11:07:29 -0700

    add .dot class

--- web/src/lib/util/svelte/BrowserFrame.svelte
+++ web/src/lib/util/svelte/BrowserFrame.svelte
@@ -13,13 +13,9 @@
       d="M4 0h431c2.209139 0 4 1.790861 4 4v9H0V4c0-2.209139 1.790861-4 4-4z"
       fill={color}
     />
-    <circle fill="#FF6158" cx="6.5" cy="6.5" r="2.5" />
-    <circle fill="#FFBE2D" cx="22.5" cy="6.5" r="2.5" /><circle
-      fill="#27C93F"
-      cx="14.5"
-      cy="6.5"
-      r="2.5"
-    />
+    <circle fill="#FF6158" cx="6.5" cy="6.5" r="2.5" class="dot" />
+    <circle fill="#FFBE2D" cx="22.5" cy="6.5" r="2.5" class="dot" />
+    <circle fill="#27C93F" cx="14.5" cy="6.5" r="2.5" class="dot" />
   </g>
 </svg>
 
