3f67cce356ae2e5b471096820a3d45990a051031
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-17 14:06:04 -0700

    Query listener

--- web/src/lib/sanity/client.ts
+++ web/src/lib/sanity/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { groqStore } from '@sanity/groq-store';
 
 const options = {
   dataset: 'production',
@@ -9,6 +10,15 @@ const options = {
   apiVersion: '2023-06-02',
   withCredentials: true
 };
+
+export const sanityStore = groqStore({
+  dataset: 'production',
+  projectId: 'n1wxk3oc',
+  listen: true,
+  overlayDrafts: true,
+  documentLimit: 10000,
+  includeTypes: ['caseStudy']
+});
 
 export const client = createClient(options);
 
--- web/src/routes/[slug]/+page.svelte
+++ web/src/routes/[slug]/+page.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
-  import type { CaseStudyQuery } from '$lib/sanity/queries';
+  import { caseStudyQuery, type CaseStudyQuery } from '$lib/sanity/queries';
+
   import ProjectPage from '$lib/ProjectPage.svelte';
+  import { onMount, onDestroy } from 'svelte';
+  import { sanityStore } from '$lib/sanity/client';
+  import type { Subscription } from '@sanity/groq-store';
+
   export let data: CaseStudyQuery;
+
+  let sub: Subscription | undefined;
+
+  onMount(async () => {
+    if (!data.slug) return;
+
+    const params = { slug: data.slug };
+
+    function handleSub(err: Error | undefined, res: unknown) {
+      if (err) {
+        console.error(err);
+      } else {
+        const caseStudys = res as CaseStudyQuery[];
+        data = caseStudys[0];
+      }
+    }
+
+    sub = sanityStore.subscribe(caseStudyQuery, params, handleSub);
+  });
+
+  onDestroy(() => {
+    sub?.unsubscribe();
+  });
 </script>
 
 <ProjectPage {data} />
