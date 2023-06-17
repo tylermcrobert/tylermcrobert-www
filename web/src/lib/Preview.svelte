2b8f41b216ccc91caa607c452a4bd042253fe5d9
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-17 16:15:07 -0700

    Add preview component

---
+++ web/src/lib/Preview.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+  import { sanityStore } from '$lib/sanity/client';
+  import type { Subscription } from '@sanity/groq-store';
+  import type { QueryParams } from '@sanity/client';
+
+  export let query: string;
+  export let params: QueryParams;
+  export let onUpdate: (cb: any) => void;
+
+  let loading = true;
+  let sub: Subscription | undefined;
+
+  onMount(async () => {
+    function handleSub(err: Error | undefined, res: unknown[] | undefined) {
+      if (err) {
+        console.error(err);
+      } else {
+        loading = false;
+
+        if (res && res[0]) {
+          onUpdate(res[0]);
+        } else {
+          console.error('Response not found');
+        }
+      }
+    }
+
+    sub = sanityStore.subscribe(query, params, handleSub);
+  });
+
+  onDestroy(() => {
+    sub?.unsubscribe();
+  });
+</script>
+
+{#if loading}
+  <div class="loadingIndicator">loading preview...</div>
+{/if}
+
+<div class="previewIndicator">
+  <span>Preview Mode</span>
+  <button>✕</button>
+</div>
+
+<slot />
+
+<style>
+  .loadingIndicator {
+    position: fixed;
+    bottom: var(--space-standard);
+    right: var(--space-standard);
+    z-index: var(--z-preview-overlay);
+
+    height: 100vh;
+    width: 100vw;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    background: white;
+  }
+
+  .previewIndicator {
+    font-size: 0.8em;
+
+    position: fixed;
+    bottom: var(--space-standard);
+    right: var(--space-standard);
+    z-index: var(--z-preview-overlay);
+
+    display: flex;
+    gap: 0.5em;
+    align-items: center;
+
+    border-radius: 5em;
+
+    background: orange;
+    padding: 0.5em 1em;
+  }
+
+  button {
+    font-size: 0.7rem;
+
+    cursor: pointer;
+
+    border: 1px solid black;
+    width: 1.5em;
+    height: 1.5em;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 999px;
+  }
+</style>
--- web/src/routes/[slug]/+page.svelte
+++ web/src/routes/[slug]/+page.svelte
@@ -1,35 +1,15 @@
 <script lang="ts">
   import { caseStudyQuery, type CaseStudyQuery } from '$lib/sanity/queries';
-
   import ProjectPage from '$lib/ProjectPage.svelte';
-  import { onMount, onDestroy } from 'svelte';
-  import { sanityStore } from '$lib/sanity/client';
-  import type { Subscription } from '@sanity/groq-store';
+  import Preview from '$lib/Preview.svelte';
 
   export let data: CaseStudyQuery;
-
-  let sub: Subscription | undefined;
-
-  onMount(async () => {
-    if (!data.slug) return;
-
-    const params = { slug: data.slug };
-
-    function handleSub(err: Error | undefined, res: unknown) {
-      if (err) {
-        console.error(err);
-      } else {
-        const caseStudys = res as CaseStudyQuery[];
-        data = caseStudys[0];
-      }
-    }
-
-    sub = sanityStore.subscribe(caseStudyQuery, params, handleSub);
-  });
-
-  onDestroy(() => {
-    sub?.unsubscribe();
-  });
 </script>
 
-<ProjectPage {data} />
+<Preview
+  query={caseStudyQuery}
+  params={{ slug: data.slug }}
+  onUpdate={(newData) => (data = newData)}
+>
+  <ProjectPage {data} />
+</Preview>
