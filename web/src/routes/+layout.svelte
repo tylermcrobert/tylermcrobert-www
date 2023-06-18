b06d7536a35121b7a99225f53b2a89d8efc3c814
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-17 22:34:19 -0700

    Fix page title

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
   import '../styles/globals.scss';
   import Nav from '$lib/Nav.svelte';
-  import Head from '$lib/util/svelte/Head.svelte';
-  import type { PageData } from './$types';
-
-  export let data: PageData;
 </script>
 
-<Head pageTitle={null} description={data.bio} route="/" />
 <Nav />
 <slot />
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { page } from '$app/stores';
   import HomeIndex from '$lib/HomeIndex.svelte';
+  import Head from '$lib/util/svelte/Head.svelte';
 </script>
 
+<Head pageTitle={null} description={$page.data.bio} route="/" />
 <HomeIndex />
