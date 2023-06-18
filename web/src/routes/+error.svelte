56b6f3f2876ef7dac14b7585a898ade124dbd08b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-17 21:36:17 -0700

    Throw not found

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  console.log();
+</script>
+
+<div class="err">
+  <p>{$page.status}</p>
+  <p>{$page.error?.message}</p>
+</div>
+
+<style>
+  .err {
+    height: calc(100vh - var(--nav-height));
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    justify-content: space-evenly;
+  }
+</style>
--- web/src/routes/[slug]/+page.ts
+++ web/src/routes/[slug]/+page.ts
@@ -1,5 +1,6 @@
 import { client } from '$lib/sanity/client';
 import { caseStudyQuery } from '$lib/sanity/queries';
+import { error } from '@sveltejs/kit';
 
 export async function load(ctx) {
   // TODO: Catch 404s
@@ -7,6 +8,11 @@ export async function load(ctx) {
     slug: ctx.params.slug
   });
 
+  if (!responses || !responses.length) {
+    throw error(404, {
+      message: 'Not found'
+    });
+  }
   return {
     ...ctx.data,
     // Add draft if authenticated
