0096442b1d41a3056d147bc906be9ce71b48ff5f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-17 17:58:28 -0700

    add tood

--- web/src/routes/[slug]/+page.ts
+++ web/src/routes/[slug]/+page.ts
@@ -2,6 +2,7 @@ import { client } from '$lib/sanity/client';
 import { caseStudyQuery } from '$lib/sanity/queries';
 
 export async function load(ctx) {
+  // TODO: Catch 404s
   const responses = await client.fetch(caseStudyQuery, {
     slug: ctx.params.slug
   });
