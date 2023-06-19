3a4459fa28622a3e94a6e59c536ae7de63a7a3c3
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-19 12:30:55 -0700

    Add "see more" to playlists

--- web/src/routes/playlists/+page.svelte
+++ web/src/routes/playlists/+page.svelte
@@ -1,40 +1,51 @@
 <script lang="ts">
-  // TODO: Add "see more"
   // TODO: Update details for mobile
+
+  let LIMIT = 5;
 
   import type { SpotifyPlaylist } from '$lib/spotify/spotify';
   import DotHead from '$lib/util/svelte/DotHead.svelte';
   import { NUMS } from '../../constants';
 
+  let itemOpen: number | null = null;
+
+  function handleOpen(index: number) {
+    if (index === itemOpen) itemOpen = null;
+    else itemOpen = index;
+  }
+
   export let data: { playlists: SpotifyPlaylist[] };
 </script>
 
-{#each data.playlists as { name, tracks, image, date, href, duration }}
+{#each data.playlists as { name, tracks, image, date, href, duration }, playlistIndex}
+  {@const isActive = playlistIndex === itemOpen}
+
   <section class="wrapper playlist">
     <div class="title h1">
       <img src={image} alt={name} class="playlistImage" />
-      <h2>
-        {name}
-      </h2>
+      <h2>{name}</h2>
     </div>
 
     <div class="details grid">
       <div class="detailItem"><DotHead>{date}</DotHead></div>
       <div class="detailItem"><DotHead>DUR {duration}</DotHead></div>
       <div class="detailItem">
-        <a {href} target="_blank"> <DotHead>Link ↗</DotHead></a>
+        <a {href} target="_blank"><DotHead>LINK ↗</DotHead></a>
       </div>
     </div>
 
     <ul class="h1 playlistTracks">
       {#each tracks as { name, duration, artists }, i}
-        <li class="playlistTrack">
+        <li class="playlistTrack" class:hidden={i + 1 > LIMIT && !isActive}>
           {NUMS[i + 1]}
-          {name} &ndash;
-          {artists.join(' & ')} ({duration}){' '}
+          {name}&mdash;{artists.join(' & ')} ({duration}){' '}
         </li>
       {/each}
     </ul>
+
+    <button on:click={() => handleOpen(playlistIndex)}>
+      <DotHead>{isActive ? 'See Less ↑' : 'See All ↓'}</DotHead>
+    </button>
   </section>
 {/each}
 
@@ -71,5 +82,9 @@
 
   .playlistTrack {
     display: inline;
+
+    &.hidden {
+      display: none;
+    }
   }
 </style>
