988efdd0269eb8519aa6755188ccedc5407d28c1
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-19 20:26:01 -0700

    Get audio started

--- web/src/routes/track/[slug]/+page.server.ts
+++ web/src/routes/track/[slug]/+page.server.ts
@@ -1,13 +1,8 @@
 import type { PageServerLoad } from './$types';
-import { getAudioDurationInSeconds } from 'get-audio-duration';
-import { formatTime } from '$lib/util/msToTime';
 
 const MP3 =
   'https://cdn.sanity.io/files/n1wxk3oc/production/a22773b70b09933742e5a2bc52b01c3409c97ac8.mp3';
 
 export const load = (async () => {
-  const seconds = await getAudioDurationInSeconds(MP3);
-  const duration = formatTime(seconds * 1000, 'mm:ss');
-
-  return { duration };
+  return { link: MP3 };
 }) satisfies PageServerLoad;
--- web/src/routes/track/[slug]/+page.svelte
+++ web/src/routes/track/[slug]/+page.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
-  export let data: { duration: string };
+  import { formatTime } from '$lib/util/msToTime';
+
+  export let data: { link: string };
+
+  let duration: number;
+  let time = 0;
+
+  $: progress = Math.round((time / duration) * 10000) / 100;
+  $: scrubberStyle = `transform: translate3d(${progress}%, 0, 0)`;
 </script>
 
-{data.duration}
+<audio controls bind:duration bind:currentTime={time}>
+  <source src={data.link} />
+</audio>
+
+<div class="scrubberWrapper">
+  <div class="scrubber" style={scrubberStyle} />
+</div>
+
+{#if duration}
+  {formatTime(time * 1000, 'mm:ss')} /
+  {formatTime(duration * 1000, 'mm:ss')}
+{/if}
+
+<style lang="scss">
+  .scrubberWrapper {
+    height: 1rem;
+    width: 10rem;
+    border: 1px solid blue;
+    position: relative;
+    margin: 1rem;
+  }
+
+  .scrubber {
+    height: 100%;
+    left: 0;
+    border-left: 1px solid red;
+  }
+</style>
