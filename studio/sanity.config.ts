425dc0489058ed9bbf2cd00e5a3aa100a01f913d
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-06-21 15:55:55 -0700

    Remove sanity-plugin-media

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -4,7 +4,6 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import {colorInput} from '@sanity/color-input'
 import {DocumentIcon} from '@sanity/icons'
-import {media} from 'sanity-plugin-media'
 
 export default defineConfig({
   name: 'default',
@@ -32,7 +31,6 @@ export default defineConfig({
           ]),
     }),
     visionTool(),
-    media(),
     colorInput(),
   ],
 
