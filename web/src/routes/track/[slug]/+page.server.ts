66ced1f24731d1945b364a9b483bf62af48369eb
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-02-26 11:33:33 -0800

    feat: throw 404 if not found

--- web/src/routes/track/[slug]/+page.server.ts
+++ web/src/routes/track/[slug]/+page.server.ts
@@ -2,6 +2,7 @@ import { createClient } from '@sanity/client';
 import type { PageServerLoad } from './$types';
 import groq from 'groq';
 import { SNIPPETS_TOKEN } from '$env/static/private';
+import { error } from '@sveltejs/kit';
 
 const snippetsClient = createClient({
   projectId: '4uxl8u8u',
@@ -31,6 +32,10 @@ export const load = (async (ctx) => {
   const track: TrackQuery = await snippetsClient.fetch(trackQuery, {
     slug: ctx.params.slug
   });
+
+  if (!track) {
+    throw error(404, 'not found');
+  }
 
   return track;
 }) satisfies PageServerLoad;
