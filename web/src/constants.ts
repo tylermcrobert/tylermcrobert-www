a694fb6a081fafaf04507366b293de1efa23bfab
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-29 13:39:16 -0700

    feat: use svelte-sanity-image

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -53,15 +53,7 @@ export const NUMS = [
   '㊿'
 ];
 
-const initial = [640, 750, 828, 1080, 1200, 1400, 1600];
-const all = [...initial, ...initial.map((item) => item * 2)];
-const sorted = all.sort((a, b) => a / b);
-
-export const IMG_DEVICE_SIZES = sorted;
-
 export const EMAIL = 'hello@tylermcrobert.com';
 export const LINK_EMAIL = 'mailto:hello@tylermcrobert.com';
 export const IG = '@tylermcrobert';
 export const LINK_IG = 'https://www.instagram.com/tylermcrobert/';
-
-//
--- web/src/lib/components/ResponsiveImage.svelte
+++ web/src/lib/components/ResponsiveImage.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
-  import { urlFor } from '../sanity/client';
-  import { IMG_DEVICE_SIZES } from '../../constants';
+  import { client } from '../sanity/client';
   import type { SanityImage } from '../sanity/types';
-  import getImageDimensions from '../sanity/lib/getImageDimensions';
-
-  /**
-   * Halfway through standard and retna
-   */
-  const RESOLUTION = 1.5;
+  import Image from '@tylermcrobert/svelte-sanity-image';
 
   export let image: SanityImage;
   export let alt: string;
@@ -15,44 +9,15 @@
   export let aspect: number | undefined = undefined;
   export let sizes: string;
   export let quality = 75;
-  export let color: string | undefined = undefined;
   export let className: string | undefined = '';
-
-  $: enforcedAspect = aspect; // rename to be more clear
-
-  /**
-   * Create an srcset for responsive image
-   */
-
-  $: srcset = IMG_DEVICE_SIZES.map((size) => {
-    let builder = urlFor(image).width(size).auto('format').quality(quality);
-
-    if (enforcedAspect) {
-      builder = builder.height(Math.round(size / enforcedAspect));
-    }
-
-    return `${builder.url()} ${Math.round(size / RESOLUTION)}w`;
-  }).join(', ');
-
-  /**
-   * Add inline style for img
-   */
-
-  $: getStyle = () => {
-    let styles = [];
-    if (color) styles.push(`background-color: ${color}`);
-    styles.push(
-      `aspect-ratio: ${enforcedAspect || getImageDimensions(image).aspectRatio}`
-    );
-    return styles.join('; ');
-  };
 </script>
 
-<img
-  src={urlFor(image).url()}
+<Image
+  {image}
+  {client}
   {alt}
-  {srcset}
+  {quality}
   {sizes}
-  style={getStyle()}
+  {aspect}
   class={`bg-image-preload ${className}`.trim()}
 />
