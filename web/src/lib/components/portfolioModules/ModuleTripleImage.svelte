569509202533bcd9ca4f8ac21440f82cee4aba92
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-29 12:03:57 -0700

    chore: add triple image

--- web/src/lib/components/portfolioModules/ModuleTripleImage.svelte
+++ web/src/lib/components/portfolioModules/ModuleTripleImage.svelte
@@ -6,36 +6,41 @@
   export let data: ModuleTripleImage;
 </script>
 
-<div class:right={data.imageRight} class:left={!data.imageRight}>
+<div class="grid-standard col-span-6 grid-rows-2">
   {#if data.mainImage}
-    <div>
+    <div
+      class={`col-span-3 row-span-2 overflow-hidden ${data.imageRight ? 'col-[4_/_span_3]' : ''}`.trim()}
+    >
       <ResponsiveImage
         image={data.mainImage}
         sizes="45vw"
         alt={$page.data.title}
         aspect={2 / 3}
+        className="h-full w-full object-cover"
       />
     </div>
   {/if}
 
   {#if data.secondaryImage1}
-    <div>
+    <div class="col-span-3 overflow-hidden" class:row-start-1={data.imageRight}>
       <ResponsiveImage
         image={data.secondaryImage1}
         sizes="45vw"
         alt={$page.data.title}
         aspect={3 / 2}
+        className="h-full w-full object-cover"
       />
     </div>
   {/if}
 
   {#if data.secondaryImage2}
-    <div>
+    <div class="col-span-3 overflow-hidden">
       <ResponsiveImage
         image={data.secondaryImage2}
         sizes="45vw"
         alt={$page.data.title}
         aspect={3 / 2}
+        className="h-full w-full object-cover"
       />
     </div>
   {/if}
