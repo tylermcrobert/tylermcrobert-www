9fd5840c2254c11890eedce5771eac200b5c4410
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-28 20:44:37 -0700

    chore: add tailwind

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import '../styles/globals.scss';
+  import '../styles/app.css';
   import Nav from '$lib/components/Nav.svelte';
   import WindowHeight from '$lib/components/WindowHeight.svelte';
 </script>
