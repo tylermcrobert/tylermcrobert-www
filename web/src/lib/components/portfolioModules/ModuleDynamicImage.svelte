fc7b7c58b9e21ba257ffce5cb4f5be99b81c33e2
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-29 12:05:51 -0700

    fix: dynamic image

--- web/src/lib/components/portfolioModules/ModuleDynamicImage.svelte
+++ web/src/lib/components/portfolioModules/ModuleDynamicImage.svelte
@@ -8,12 +8,13 @@
   $: half = data.span === 'half';
 </script>
 
-<div class:col-span-3={half} class:md:col-span-6={!half}>
+<div class:col-span-3={half} class:md:col-span-6={!half} class="col-span-6">
   {#if data.image?.asset}
     <ResponsiveImage
       image={data.image}
       alt={$page.data.title}
       aspect={(data.aspect && 1 / data.aspect) || undefined}
+      className="w-full"
       sizes={half ? '50vw' : '100vw'}
     />
   {/if}
