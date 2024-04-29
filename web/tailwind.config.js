bdce44e598f47191ec32998e1183796e691bb20e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-28 21:36:15 -0700

    chore: add space to tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -11,7 +11,11 @@ export default {
       spacing: {
         ['nav-height']: '2.25rem',
         wrapper: '65rem',
-        standard: '1.33rem'
+
+        standard: '1.33rem',
+        small: '0.667rem',
+        medium: '3.16rem',
+        large: '5rem'
       }
     }
   },
