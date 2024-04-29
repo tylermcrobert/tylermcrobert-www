d6b03e154966ce58d52b4afb1cb492b91fc75bd4
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-28 21:27:15 -0700

    feat: style nav

--- web/src/lib/components/Nav.svelte
+++ web/src/lib/components/Nav.svelte
@@ -5,16 +5,18 @@
   if (ctxTitle === 'Default') ctxTitle = null;
 </script>
 
-<nav>
-  <div>
+<nav class="z-nav sticky top-0 text-white mix-blend-difference">
+  <div
+    class="h-nav-height px-standard max-w-wrapper mx-auto flex items-center justify-between"
+  >
     <div>
       <a href="/">Tyler McRobert</a>
 
       {#if ctxTitle}
-        <a href="/c/default">
+        <a href="/c/default" class="group">
           &times;
           <span>{ctxTitle}</span>
-          <span>(close)</span>
+          <span class="opacity-0 group-hover:opacity-100">(close)</span>
         </a>
       {/if}
     </div>
