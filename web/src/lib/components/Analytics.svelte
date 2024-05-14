efb0bc5ef4647718baeeb7585510355516516f74
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-05-14 10:31:06 -0700

    chore: add ga

---
+++ web/src/lib/components/Analytics.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  const MEASUREMENT_ID = 'UA-72543203-1';
+  import { page } from '$app/stores';
+
+  $: {
+    if (typeof gtag !== 'undefined') {
+      gtag('config', 'MEASUREMENT_ID', {
+        page_title: document.title,
+        page_path: $page.url.pathname
+      });
+    }
+  }
+</script>
+
+<svelte:head>
+  <script
+    async
+    src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+  >
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', MEASUREMENT_ID);
+  </script>
+</svelte:head>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import '../app.css';
   import Nav from '$lib/components/Nav.svelte';
+  import Analytics from '$lib/components/Analytics.svelte';
 </script>
 
 <Nav />
 <slot />
+
+<Analytics />
