12e1a24efb4c5a744fd638725cceaa41ab8e7598
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-11-22 13:18:31 -0800

    feat: remove h4-6

--- studio/schemaTypes/objects/richText.tsx
+++ studio/schemaTypes/objects/richText.tsx
@@ -33,9 +33,6 @@ export const richText = defineType({
         {title: 'Heading 1', value: 'h1'},
         {title: 'Heading 2', value: 'h2'},
         {title: 'Heading 3', value: 'h3'},
-        {title: 'Heading 4', value: 'h4'},
-        {title: 'Heading 5', value: 'h5'},
-        {title: 'Heading 6', value: 'h6'},
         {title: 'Quote', value: 'blockquote'},
       ],
       lists: [
