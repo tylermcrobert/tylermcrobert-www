5aeea7e4f00cf3678d9d4bfbff9f27d6cea184a7
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-11-22 13:29:00 -0800

    refactor: remove ffwd

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -4,9 +4,13 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './desk'
 
+const TITLE = 'Svelte Sanity Starter'
+const STAGING_URL = 'https://example.com'
+const CREATABLE_DOCTYPES = ['page']
+
 export default defineConfig({
   name: 'default',
-  title: 'Svelte Sanity Starter',
+  title: TITLE,
 
   projectId: 'tjt0kiru',
   dataset: 'production',
@@ -18,9 +22,13 @@ export default defineConfig({
   },
 
   document: {
+    newDocumentOptions: (item) => {
+      return item.filter((item) => CREATABLE_DOCTYPES.includes(item.templateId))
+    },
+
     productionUrl: async (prev, context) => {
       const isDev = window.location.host === 'localhost:3333'
-      const baseUrl = isDev ? 'http://localhost:5173' : 'https://ffwd-www.vercel.app'
+      const baseUrl = isDev ? 'http://localhost:5173' : STAGING_URL
 
       const slug = (context.document as any)?.slug?.current
       const type = (context.document as any)._type
