7d0027aae271de340240b97d132934ff8be19282
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-14 15:06:53 -0800

    feat: error

--- web/src/sanity/client.server.ts
+++ web/src/sanity/client.server.ts
@@ -1,6 +1,12 @@
 import { client } from './client';
 import { SANITY_TOKEN } from '$env/static/private';
 
+if (!SANITY_TOKEN) {
+	throw new Error(
+		'Missing Sanity token. Please include SANITY_TOKEN to your .env file'
+	);
+}
+
 export const previewClient = client.withConfig({
 	useCdn: false, // must be false for previewDrafts
 	perspective: 'previewDrafts',
