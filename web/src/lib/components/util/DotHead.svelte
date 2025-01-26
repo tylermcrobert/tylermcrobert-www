57ee102f2f82db282518b42f00748b104ef18cf7
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-01-25 19:44:47 -0800

    chore: remove previous

--- web/src/lib/components/util/DotHead.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { DOT } from '../../../constants';
-
-  export let noMarginBottom = false;
-</script>
-
-<span class="inline-block uppercase" class:mb-4={!noMarginBottom}>
-  {DOT}&nbsp;<slot />
-</span>
--- web/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { page } from '$app/stores';
-  import HomeIndex from '$lib/components/HomeIndex.svelte';
-  import Head from '$lib/components/util/Head.svelte';
-</script>
-
-<Head pageTitle={null} description={$page.data.bio} route="/" />
-<HomeIndex />
