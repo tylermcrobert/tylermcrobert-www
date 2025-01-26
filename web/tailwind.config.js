31d01ad953fa30b6dde207ddbe23e8729e3059ce
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-01-25 18:32:04 -0800

    refactor: tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,28 +4,6 @@ const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		lineHeight: {
-			1.05: '1.05',
-			1.1: '1.1',
-			1.2: '1.2',
-			1.3: '1.3',
-			1.4: '1.4'
-		},
-		extend: {
-			zIndex: {
-				nav: '100'
-			},
-			maxWidth: {
-				'rag-heading': '25ch',
-				'rag-paragraph': '55ch'
-			},
-			spacing: {
-				'available-svh': 'calc(100svh - 60px)',
-				'available-dvh': 'calc(100dvh - 60px)'
-			}
-		}
-	},
 	plugins: [
 		plugin(({ addVariant }) => {
 			addVariant('not-last', '&:not(:last-child)');
