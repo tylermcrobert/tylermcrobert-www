da124ae96a1686bb9fbe3a38012b7989ea6b48c1
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-01-26 17:17:28 -0800

    chore: move breakpoints

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,13 +5,6 @@ const plugin = require('tailwindcss/plugin');
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		screens: {
-			sm: '640px',
-			md: '768px',
-			lg: '1024px',
-			xl: '1440px',
-			'2xl': '1900px'
-		},
 		extend: {
 			backgroundColor: {
 				'image-preload': '#f5f5f5'
