83e42245d3b27fa01bb4402a36bdd8b0637dc171
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-01-26 20:36:54 -0800

    feat: nav

--- web/src/components/Navigation.svelte
+++ web/src/components/Navigation.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	let contextTitle = $derived(page.data.contextTitle);
+</script>
+
 <nav class="z-nav sticky top-0 text-white mix-blend-difference">
 	<div
 		class="h-nav-height px-standard max-w-wrapper mx-auto flex items-center justify-between"
@@ -5,13 +11,13 @@
 		<div>
 			<a href="/">Tyler McRobert</a>
 
-			<!-- {#if ctxTitle}
-        <a href="/c/default" class="group">
-          &times;
-          <span>{ctxTitle}</span>
-          <span class="opacity-0 group-hover:opacity-100">(close)</span>
-        </a>
-      {/if} -->
+			{#if contextTitle}
+				<a href="/c/exit" class="group">
+					&times;
+					<span>{contextTitle}</span>
+					<span class="opacity-0 group-hover:opacity-100">(close)</span>
+				</a>
+			{/if}
 		</div>
 
 		<a href="/info">Info</a>
