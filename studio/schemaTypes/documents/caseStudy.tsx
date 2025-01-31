05e5c817b905639a6d1fae6eff05daeadbc71ba3
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-01-30 18:51:12 -0800

    feat: remove old modules

--- studio/schemaTypes/documents/caseStudy.tsx
+++ studio/schemaTypes/documents/caseStudy.tsx
@@ -62,16 +62,7 @@ export default defineField({
     {
       name: 'modules',
       type: 'modules',
-      deprecated: {
-        reason: 'Use Modules V2',
-      },
       validation: (Rule) => Rule.required(),
-    },
-
-    {
-      name: 'modulesV2',
-      type: 'modules',
-      // validation: (Rule) => Rule.required(),
     },
 
     {
