2303d7037f52a88b0f08b550f5a278f48900b569
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-01-30 18:36:00 -0800

    chore: use refactor dataset

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: 'production',
+    dataset: 'refactor-2025',
   },
 })
--- web/src/lib/sanity/client.ts
+++ web/src/lib/sanity/client.ts
@@ -3,7 +3,7 @@ import { PUBLIC_SANITY_PROJECT_ID } from '$env/static/public';
 
 export const client = createClient({
 	projectId: PUBLIC_SANITY_PROJECT_ID,
-	dataset: 'production',
+	dataset: 'refactor-2025',
 	useCdn: process.env.NODE_ENV === 'production',
 	apiVersion: '2024-04-20'
 });
