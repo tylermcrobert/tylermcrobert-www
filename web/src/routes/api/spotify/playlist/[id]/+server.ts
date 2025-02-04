86db12633a00c367882cf0f67dc22330dc6b4333
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-02-04 15:13:09 -0800

    refactor: spotify -> api route

--- web/src/routes/api/spotify/playlist/[id]/+server.ts
+++ web/src/routes/api/spotify/playlist/[id]/+server.ts
@@ -1,5 +1,13 @@
-import { getPlaylistById } from '$lib/spotify/+spotify.js';
 import { json } from '@sveltejs/kit';
+import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from '$env/static/private';
+
+const CREDENTIALS_ENDPOINT =
+	'https://accounts.spotify.com/api/token?grant_type=client_credentials';
+const PLAYLISTS_ENDPOINT = 'https://api.spotify.com/v1/playlists';
+
+if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
+	throw new Error('Spotify API keys not found in environment variables.');
+}
 
 export async function GET({ params: { id } }) {
 	try {
@@ -7,7 +15,36 @@ export async function GET({ params: { id } }) {
 			return json({ error: 'Playlist ID is required' }, { status: 400 });
 		}
 
-		const res = await getPlaylistById(id);
+		/**
+		 * Fetch Auth code
+		 */
+
+		const authFormatted = `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`;
+		const authEncoded = Buffer.from(authFormatted).toString('base64');
+
+		const credentialsRes = await fetch(CREDENTIALS_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization: `Basic ${authEncoded}`
+			}
+		});
+
+		const credentialsData = await credentialsRes.json();
+		const accessToken = credentialsData.access_token;
+
+		if (!credentialsRes.ok || !accessToken) {
+			return json({ error: 'Failed to fetch auth token' }, { status: 500 });
+		}
+
+		/**
+		 * Fetch Playlists
+		 */
+
+		const res = await fetch(`${PLAYLISTS_ENDPOINT}/${id}`, {
+			headers: { Authorization: `Bearer ${accessToken}` }
+		});
+
 		const data = await res.json();
 
 		return json(data);
