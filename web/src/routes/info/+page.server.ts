512ab9c179a3efd1e8424d9e6bc3546a69a411dd
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-02-05 23:49:05 -0800

    feat: promise all

--- web/src/routes/info/+page.server.ts
+++ web/src/routes/info/+page.server.ts
@@ -2,10 +2,10 @@ import getNowPlaying from '$lib/last.fm/+lastfm.js';
 import { infoQuery, type InfoQuery } from '$sanity';
 
 export const load = async ({ locals: { client } }) => {
-	const [nowPlaying, infoPage] = [
+	const [nowPlaying, infoPage] = await Promise.all([
 		await getNowPlaying(),
 		await client.fetch<InfoQuery>(infoQuery)
-	];
+	]);
 
 	return {
 		nowPlaying,
