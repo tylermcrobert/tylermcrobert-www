f5b9c289b13944054715c4d071a6c3b39afc010f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-02-22 14:27:59 -0800

    fix: 404

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -2,9 +2,11 @@
 	import { page } from '$app/state';
 </script>
 
-<section class="h-available-dvh flex flex-col justify-center px-4 text-center">
+<section
+	class="h-available-dvh flex flex-col items-center justify-center px-4 text-center"
+>
 	{#if page.status === 404}
-		<div class="text-h1">404</div>
+		<div class="text-h1 mb-2">404</div>
 		<div>This page could not be found.</div>
 	{:else}
 		{page.status} - {page.error?.message}
