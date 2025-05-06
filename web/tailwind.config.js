83160e94fb59c0797f8ac2b2623d860366a7c5ed
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-06 11:55:53 -0700

    refactor: tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,15 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const plugin = require('tailwindcss/plugin');
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	plugins: [
-		plugin(({ addVariant }) => {
-			addVariant('not-last', '&:not(:last-child)');
-			addVariant('not-first', '&:not(:first-child)');
-		})
-	],
+	plugins: [],
 	future: {
 		hoverOnlyWhenSupported: true
 	}
