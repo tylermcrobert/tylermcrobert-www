93993ec65c025b510407d421b5d0796ee44ee9ad
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-06 12:00:18 -0700

    refactor: richText

--- web/src/components/RichText/RichText.svelte
+++ web/src/components/RichText/RichText.svelte
@@ -7,40 +7,37 @@
 
 	type Props = {
 		value: RichTextProjection;
-		class?: string;
 	};
 
-	let { value, class: className }: Props = $props();
+	let { value }: Props = $props();
 </script>
 
-<div class={['richtext', className]}>
-	<PortableText
-		{value}
-		components={{
-			block: {
-				normal: RichTextStyleRenderer,
-				h1: RichTextStyleRenderer,
-				h2: RichTextStyleRenderer,
-				blockquote: RichTextStyleRenderer
-			},
-			list: {
-				bullet: RichTextListRenderer,
-				number: RichTextListRenderer,
-				normal: RichTextListRenderer
-			},
-			listItem: {
-				bullet: RichTextListRenderer,
-				number: RichTextListRenderer,
-				normal: RichTextListRenderer
-			},
-			marks: {
-				link: RichTextLinkRenderer,
-				internalLink: RichTextLinkRenderer,
-				externalLink: RichTextLinkRenderer
-			},
-			types: {
-				// Components here...
-			}
-		}}
-	/>
-</div>
+<PortableText
+	{value}
+	components={{
+		block: {
+			normal: RichTextStyleRenderer,
+			h1: RichTextStyleRenderer,
+			h2: RichTextStyleRenderer,
+			blockquote: RichTextStyleRenderer
+		},
+		list: {
+			bullet: RichTextListRenderer,
+			number: RichTextListRenderer,
+			normal: RichTextListRenderer
+		},
+		listItem: {
+			bullet: RichTextListRenderer,
+			number: RichTextListRenderer,
+			normal: RichTextListRenderer
+		},
+		marks: {
+			link: RichTextLinkRenderer,
+			internalLink: RichTextLinkRenderer,
+			externalLink: RichTextLinkRenderer
+		},
+		types: {
+			// Components here...
+		}
+	}}
+/>
