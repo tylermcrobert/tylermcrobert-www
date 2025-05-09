4e3dc7bf6a8a061ba6535464a8795b50f7bb7231
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-08 23:12:36 -0700

    refactor: observer

--- web/src/lib/actions.svelte.ts
+++ web/src/lib/actions.svelte.ts
@@ -6,23 +6,57 @@ type IntersectionOptions = {
 	options?: IntersectionObserverInit;
 };
 
+// Keep track of which callback is associated with each element
+const intersectionCallbacks = new WeakMap<Element, IntersectionCallback>();
+
+// Use a single intersection observer instance per options
+const intersectionObservers = new WeakMap<
+	IntersectionObserverInit,
+	IntersectionObserver
+>();
+
+function createObserver(init: IntersectionObserverInit | undefined) {
+	const observer = new IntersectionObserver((entries) => {
+		for (const entry of entries) {
+			const callback = intersectionCallbacks.get(entry.target);
+			if (callback) {
+				callback(entry);
+			}
+		}
+	}, init);
+	if (init) {
+		intersectionObservers.set(init, observer);
+	}
+	return observer;
+}
+
+function observe(
+	target: Element,
+	props: IntersectionCallback | IntersectionOptions
+) {
+	const callback = typeof props === 'function' ? props : props.callback;
+	const options = typeof props === 'object' ? props.options : undefined;
+
+	const observer = options
+		? intersectionObservers.get(options) || createObserver(options)
+		: createObserver(undefined);
+
+	intersectionCallbacks.set(target, callback);
+	observer.observe(target);
+
+	return () => {
+		observer.unobserve(target);
+		intersectionCallbacks.delete(target);
+	};
+}
+
 export function intersect(
 	target: Element,
 	props: IntersectionCallback | IntersectionOptions
 ): void {
-	const observer = new IntersectionObserver(
-		([entry]) => {
-			if (typeof props === 'function') {
-				props(entry);
-			} else {
-				props.callback(entry);
-			}
-		},
-		typeof props === 'object' ? props.options : undefined
-	);
+	const unobserve = observe(target, props);
 
 	onMount(() => {
-		observer.observe(target);
-		return () => observer.unobserve(target);
+		return () => unobserve();
 	});
 }
