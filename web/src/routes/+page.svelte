68643c2b801575745dd5b274f418bca144a96d0e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-09 09:12:16 -0700

    chore: remove imports

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { Modules } from '$components';
-	import { intersect } from '$lib/actions.svelte';
 	let { data } = $props();
-	let intersecting = $state(false);
 </script>
 
 <Modules modules={data.modules} />
