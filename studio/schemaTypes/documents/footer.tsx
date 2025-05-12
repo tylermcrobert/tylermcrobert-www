bd364cba75ae1f4a6af49de4601c4685273c26ee
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-12 14:54:17 -0700

    fix: hide footer field

--- studio/schemaTypes/documents/footer.tsx
+++ studio/schemaTypes/documents/footer.tsx
@@ -11,7 +11,7 @@ export default defineType({
       name: 'title',
       type: 'string',
       initialValue: 'Footer',
-      // hidden:true,
+      hidden: true,
     },
     {
       name: 'links',
