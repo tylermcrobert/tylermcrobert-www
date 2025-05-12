f6fc343ee6f81ee06953ae9946a9ce87d2fe8ea0
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-12 14:47:56 -0700

    fix: structure

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   plugins: [
     structureTool({
       title: 'Content',
-      ...structure,
+      structure,
     }),
     presentationTool({
       title: 'Preview',
