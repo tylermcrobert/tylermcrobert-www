fcdf35865bb364793222f1369dc867b44fbabd98
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-12 19:38:56 -0700

    feat: indent

--- web/src/components/RichText/RichTextStyleRenderer.svelte
+++ web/src/components/RichText/RichTextStyleRenderer.svelte
@@ -22,7 +22,7 @@
 		class={{
 			'text-h1': style === 'h1',
 			'text-h2': style === 'h2',
-			'text-para indent': style === 'normal',
+			'text-para first-of-type:indent-8': style === 'normal',
 			'border-l border-current pl-3': style === 'blockquote'
 		}}
 	>
