cffafd0fe5f4734d91232d06ed79226f01de7bd1
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-09-17 09:59:57 -0700

    refactor: h1

--- web/src/components/RichText/RichTextStyleRenderer.svelte
+++ web/src/components/RichText/RichTextStyleRenderer.svelte
@@ -24,7 +24,7 @@
 	<svelte:element
 		this={elementName}
 		class={{
-			'text-h1': style === 'h1',
+			'text-h1 max-w-[17ch]': style === 'h1',
 			'text-h2': style === 'h2',
 			'text-para first-of-type:indent-8': style === 'normal',
 			'border-l border-current pl-3': style === 'blockquote',
