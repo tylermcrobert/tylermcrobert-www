046bc4bb56ff263efcbc1545f47d8993b2bd6bed
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-11-03 22:59:58 -0800

    refactor: more elegant hook

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,5 +1,5 @@
 import { handlePreviewMode } from '@sanity/sveltekit';
-import { redirect, type RequestEvent } from '@sveltejs/kit';
+import { type Handle, redirect } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
 import { PREVIEW_COOKIE_SECRET } from '$env/static/private';
@@ -7,34 +7,26 @@ import { serverClient } from '$sanity/client.server';
 
 export const PREVIEW_COOKIE_NAME = 'preview_mode_secret';
 
-/**
- * Determins if stega should be enabled.
- * By default it should not ship to the browser unless:
- *  - The user is previewing a draft
- *  - The user is not using Firefox (because it creates a bug with flexbox)
- * @returns boolean
- */
-function getStegaEnabledState(event: RequestEvent) {
-	const isPreview =
-		event.cookies.get(PREVIEW_COOKIE_NAME) === PREVIEW_COOKIE_SECRET;
-	const isFirefox = event.request.headers
+const previewWithStegaLogic: Handle = async ({ event, resolve }) => {
+	const previewCookie = event.cookies.get(PREVIEW_COOKIE_NAME);
+	const isPreview = previewCookie === PREVIEW_COOKIE_SECRET;
+
+	// Firefox has layout issues with stega
+	const notFirefox = !event.request.headers
 		.get('user-agent')
 		?.toLowerCase()
 		.includes('firefox');
 
-	return isPreview && !isFirefox;
-}
+	return handlePreviewMode({
+		client: serverClient.withConfig({
+			stega: { enabled: isPreview && notFirefox }
+		}),
+		preview: {
+			redirect,
+			cookie: PREVIEW_COOKIE_NAME,
+			secret: PREVIEW_COOKIE_SECRET
+		}
+	})({ event, resolve });
+};
 
-export const handle = (ctx) =>
-	sequence(
-		handlePreviewMode({
-			client: serverClient.withConfig({
-				stega: { enabled: getStegaEnabledState(ctx.event) }
-			}),
-			preview: {
-				redirect,
-				cookie: PREVIEW_COOKIE_NAME,
-				secret: PREVIEW_COOKIE_SECRET
-			}
-		})
-	)(ctx);
+export const handle = sequence(previewWithStegaLogic);
