998d5931ee11728e5a14129a6346c4b64c75ec10
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-12-25 13:33:14 -0500

    feat: hell yeah

--- web/src/components/ScrollingBrowserStyle.svelte
+++ web/src/components/ScrollingBrowserStyle.svelte
@@ -1,9 +1,10 @@
 <script>
-	const ASPECT = 100 / 400;
+	const ASPECT = 8192 / 1429;
 </script>
 
 <div class="bg-black px-20">
 	<div class="parent relative bg-green-500" style:--image-aspect={ASPECT}>
+		<div class="height-test absolute z-10 w-full bg-red-500">asdf</div>
 		<div
 			class="sticky top-0 flex w-full items-center justify-center bg-red-500/50"
 		>
@@ -17,8 +18,15 @@
 <style>
 	.parent {
 		container-type: inline-size;
+
 		--video-height: calc(100cqi * 9 / 16);
+		--image-height: calc(100cqi * var(--image-aspect));
 		--height-minus-video: calc(100lvh - var(--video-height));
+		--total-height: calc(var(--height-minus-video) + var(--image-height));
+	}
+
+	.height-test {
+		height: var(--total-height);
 	}
 
 	.shim {
