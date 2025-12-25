07e0107264f250341b089be1d308137eabed7608
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-12-25 13:25:30 -0500

    feat: cooking

---
+++ web/src/components/ScrollingBrowserStyle.svelte
@@ -0,0 +1,29 @@
+<script>
+	const ASPECT = 100 / 400;
+</script>
+
+<div class="bg-black px-20">
+	<div class="parent relative bg-green-500" style:--image-aspect={ASPECT}>
+		<div
+			class="sticky top-0 flex h-lvh w-full items-center justify-center bg-red-500/50"
+		>
+			<div class="aspect-video w-full bg-blue-500">asdf</div>
+		</div>
+
+		<div class="shim absolute inset-x-0 top-0 h-100 bg-red-500">
+			<div>asdf</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.parent {
+		container-type: inline-size;
+		--video-height: calc(100cqi * 9 / 16);
+		--height-minus-video: calc(100lvh - var(--video-height));
+	}
+
+	.shim {
+		height: calc(var(--height-minus-video) / 2);
+	}
+</style>
