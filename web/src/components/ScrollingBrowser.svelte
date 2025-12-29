c46b2c74880ee1569ccf0a1f9c64a9a51af5f6b5
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-12-29 11:47:33 -0500

    refactor: css only

--- web/src/components/ScrollingBrowser.svelte
+++ web/src/components/ScrollingBrowser.svelte
@@ -2,55 +2,18 @@
 	import BrowserFrame from './BrowserFrame.svelte';
 	import type { MediaProjection } from '$sanity';
 	import Media from './Media.svelte';
-	import type { Attachment } from 'svelte/attachments';
-	import { scrollY } from 'svelte/reactivity/window';
-	import { intersection } from '$lib/attachments';
 
 	type Props = {
 		aspect: number;
 		asset: NonNullable<MediaProjection['asset']>;
 	};
 	let { aspect, asset }: Props = $props();
-
-	let progress = $state(0);
-	let isIntersecting = $state(false);
-
-	const scroller: Attachment = (element) => {
-		let frameId: number;
-		let running = true;
-
-		function updateProgress() {
-			if (!running) return;
-			const rect = element.getBoundingClientRect();
-			const progressUnclamped = -rect.top / (rect.height - window.innerHeight);
-			const progressClamped = Math.max(Math.min(progressUnclamped, 1), 0);
-			progress = progressClamped;
-
-			frameId = requestAnimationFrame(updateProgress);
-		}
-
-		$effect(() => {
-			if (isIntersecting) {
-				frameId = requestAnimationFrame(updateProgress);
-			} else {
-				cancelAnimationFrame(frameId);
-			}
-		});
-
-		return () => {
-			running = false;
-			cancelAnimationFrame(frameId);
-		};
-	};
 </script>
 
 <div
-	style:--progress={progress}
 	style:--image-aspect={aspect}
 	style:--frame-aspect={16 / 9}
-	{@attach scroller}
-	{@attach intersection((e) => (isIntersecting = e.isIntersecting))}
-	class="scrollweb-container bg-black px-[10%]"
+	class="scrollweb-wrapper bg-black px-[10%]"
 >
 	<div class="@container">
 		<div class="h-(--scroll-distance)">
@@ -72,32 +35,33 @@
 	</div>
 </div>
 
-<!-- prettier-ignore -->
 <style>
-	.scrollweb-container {
+	@keyframes scroll-image {
+		from {
+			transform: translateY(0);
+		}
+		to {
+			transform: translateY(var(--scroll-offset));
+		}
+	}
 
-    /* 
-    Scroll distance
-    */
+	.scrollweb-wrapper {
+		view-timeline-name: --scroll-timeline;
 
-		/* Browser frame height: { width of container } / { aspect ratio of the browser frame } */
+		/* Browser frame height: { width of wrapper } / { aspect ratio of the browser frame } */
 		--calc-browser-frame-height: calc(100cqi / var(--frame-aspect));
-		/* Y Negative Space: { height of the container } - { height of the browser frame } */
+		/* Y Negative Space: { height of the wrapper } - { height of the browser frame } */
 		--calc-y-negative-space: calc(100lvh - var(--calc-browser-frame-height));
 		/* Final Value */
-		--scroll-distance: calc((100cqi / var(--image-aspect)) + var(--calc-y-negative-space)
+		--scroll-distance: calc(
+			(100cqi / var(--image-aspect)) + var(--calc-y-negative-space)
 		);
+	}
 
-		/* 
-    Image Offset
-    */
-
-		/* This translates the image -100% but completely off the screen */
-		--calc-img-total-offset: calc(-100% * var(--progress));
-		/* This translates the browser frame down by the height of the browser frame */
-		--calc-img-browser-frame-offset: calc((var(--calc-browser-frame-height) * var(--progress)));
-		/* Final Image Offset */
-		--image-offset: calc(var(--calc-img-total-offset) + var(--calc-img-browser-frame-offset)
-		);
+	.scrollweb-wrapper :global(img) {
+		--scroll-offset: calc(-100% + var(--calc-browser-frame-height));
+		animation: scroll-image linear forwards;
+		animation-timeline: --scroll-timeline;
+		animation-range: contain;
 	}
 </style>
