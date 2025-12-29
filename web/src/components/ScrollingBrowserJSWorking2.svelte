28e2bd4e6e2772e089ec67269699493bfab6983c
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-12-28 23:44:19 -0500

    feat: better working

--- web/src/components/ScrollingBrowserJSWorking2.svelte
+++ web/src/components/ScrollingBrowserJSWorking2.svelte
@@ -2,70 +2,76 @@
 	import BrowserFrame from './BrowserFrame.svelte';
 	import type { MediaProjection } from '$sanity';
 	import Media from './Media.svelte';
+	import type { Attachment } from 'svelte/attachments';
+	import { scrollY } from 'svelte/reactivity/window';
 
 	type Props = {
-		height: number;
 		aspect: number;
 		asset: NonNullable<MediaProjection['asset']>;
 	};
-	let { height, aspect, asset }: Props = $props();
+	let { aspect, asset }: Props = $props();
 
-	let browserFrameSize = $state(0);
-	let scrollerRef = $state<HTMLElement | null>(null);
 	let progress = $state(0);
 
-	function handleScroll() {
-		if (!scrollerRef) {
-			throw new Error('Cant find scroller ref');
-		}
-		const rect = scrollerRef.getBoundingClientRect();
+	const scroller: Attachment = (element) => {
+		scrollY.current;
+
+		const rect = element.getBoundingClientRect();
 		const progressUnclamped = -rect.top / (rect.height - window.innerHeight);
 		const progressClamped = Math.max(Math.min(progressUnclamped, 1), 0);
 		progress = progressClamped;
-	}
+	};
 </script>
 
-<svelte:window onscroll={handleScroll} />
-
-<!-- Contained -->
-{height}
 <div
-	class="parent relative h-(--total-height) bg-black px-[10%]"
 	style:--progress={progress}
-	style:--image-height={height}
-	style:--browser-frame-size={browserFrameSize}
-	bind:this={scrollerRef}
+	style:--image-aspect={aspect}
+	style:--frame-aspect={16 / 9}
+	{@attach scroller}
+	class="scrollweb-container bg-black px-[10%]"
 >
-	<div class="sticky top-0 grid h-lvh items-center justify-center">
-		<div
-			class="browser-frame aspect-video w-full overflow-hidden rounded-md"
-			bind:offsetHeight={browserFrameSize}
-		>
-			<Media
-				value={asset}
-				sizes="100vw"
-				alt={null}
-				class="translate-y-(--image-offset)"
-			/>
+	<div class="@container">
+		<div class="h-(--scroll-distance)">
+			<div class="sticky top-0 grid h-lvh place-items-center">
+				<div class="grid aspect-(--frame-aspect) overflow-hidden rounded-md">
+					<Media
+						value={asset}
+						sizes="100vw"
+						alt={null}
+						class="translate-y-(--image-offset)"
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
 
+<!-- prettier-ignore -->
 <style>
-	.parent {
-		--speed: 0.3;
-		height: calc(var(--image-height) * var(--speed) * 1px);
+	.scrollweb-container {
 
-		/* prettier-ignore */
-		--transform-full: calc(var(--progress) * -100%);
-		/* prettier-ignore */
-		--transform-window-offset: calc(var(--progress) * (var(--browser-frame-size)));
+    /* 
+    Scroll distance
+    */
 
-		/* asdf */
-		/* prettier-ignore */
-		--image-offset: calc((var(--transform-full) + var(--transform-window-offset) * 1px));
-	}
+		/* Browser frame height: { width of container } / { aspect ratio of the browser frame } */
+		--calc-browser-frame-height: calc(100cqi / var(--frame-aspect));
+		/* Y Negative Space: { height of the container } - { height of the browser frame } */
+		--calc-y-negative-space: calc(100lvh - var(--calc-browser-frame-height));
+		/* Final Value */
+		--scroll-distance: calc((100cqi / var(--image-aspect)) + var(--calc-y-negative-space)
+		);
 
-	.browser-frame {
+		/* 
+    Image Offset
+    */
+
+		/* This translates the image -100% but completely off the screen */
+		--calc-img-total-offset: calc(-100% * var(--progress));
+		/* This translates the browser frame down by the height of the browser frame */
+		--calc-img-browser-frame-offset: calc((var(--calc-browser-frame-height) * var(--progress)));
+		/* Final Image Offset */
+		--image-offset: calc(var(--calc-img-total-offset) + var(--calc-img-browser-frame-offset)
+		);
 	}
 </style>
