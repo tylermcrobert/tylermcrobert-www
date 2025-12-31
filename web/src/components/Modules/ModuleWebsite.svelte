9ac38883392857ee59c9264c71949d382cc20f72
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-12-30 16:03:33 -0800

    feat: put back regular browser

--- web/src/components/Modules/ModuleWebsite.svelte
+++ web/src/components/Modules/ModuleWebsite.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import { createContext } from 'svelte';
 	import BrowserFrame from '$components/BrowserFrame.svelte';
 	import Image from '$components/Image.svelte';
 	import Media from '$components/Media.svelte';
 	import type { ModuleWebsite } from '$sanity';
-	import { colorToString } from '$util/colorToRgba';
-	import { setContext } from 'svelte';
-	import ScrollingBrowser from '$components/ScrollingBrowser.svelte';
-	import { getImageDimensions } from '@tylermcrobert/svelte-sanity-image';
 
 	type Props = { data: ModuleWebsite };
 
@@ -15,10 +10,36 @@
 	let { showFrame, backgroundImg, media } = $derived(data);
 </script>
 
-<div class="-mx-standard">
-	{#if media?.asset && media?.asset?._type === 'image'}
-		{@const { width, height } = getImageDimensions(media?.asset.image)}
+<div
+	style:background={data.backgroundColor || 'var(--browser-section-background)'}
+	class="relative my-standard w-full bg-black p-[10%]"
+>
+	<div
+		class={[
+			'relative z-10',
+			showFrame !== false && 'overflow-hidden rounded-xs md:rounded-sm'
+		]}
+	>
+		{#if showFrame !== false}
+			<BrowserFrame />
+		{/if}
 
-		<ScrollingBrowser aspect={width / height} asset={media?.asset} />
+		{#if media?.asset}
+			<div class="bg-white">
+				<Media sizes="80vw" value={media.asset} alt={null} />
+			</div>
+		{/if}
+	</div>
+
+	{#if backgroundImg}
+		<div>
+			<Image
+				image={backgroundImg}
+				alt={null}
+				sizes="90vw"
+				aspect={1.5}
+				class="absolute inset-0 z-0 h-full w-full object-cover"
+			/>
+		</div>
 	{/if}
 </div>
