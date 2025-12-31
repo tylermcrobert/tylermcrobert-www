3cef26b4ae69dc8f10a597722af307edbf971fad
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-12-30 17:53:29 -0800

    fix: styling

--- web/src/components/Modules/ModuleWebsite.svelte
+++ web/src/components/Modules/ModuleWebsite.svelte
@@ -16,7 +16,7 @@
 
 <div
 	style:background={data.backgroundColor || 'var(--browser-section-background)'}
-	class="relative my-standard w-full bg-black p-[10%]"
+	class="relative my-standard flow-root w-full bg-black px-[10%]"
 >
 	{#if !IS_SCROLLING}
 		{@render standard()}
@@ -33,7 +33,7 @@
 {#snippet standard()}
 	<div
 		class={[
-			'relative z-10',
+			'relative z-10 my-[10%]',
 			showFrame !== false && 'overflow-hidden rounded-xs md:rounded-sm'
 		]}
 	>
