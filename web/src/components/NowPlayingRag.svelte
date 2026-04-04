eadca60e4e6664db79061681bfb3deaaf52fbaf9
Tyler McRobert <tyler.mcrobert@gmail.com>
2026-04-04 09:52:12 -0700

    feat: center album art

--- web/src/components/NowPlayingRag.svelte
+++ web/src/components/NowPlayingRag.svelte
@@ -28,7 +28,7 @@
 			<img
 				src={nowPlaying.artwork.replace('{w}', '300').replace('{h}', '300')}
 				alt="Apple Music"
-				class="relative -top-[0.05em] left-[0.05em] inline-block size-[0.85em] object-cover"
+				class="relative -top-[0.085em] left-[0.05em] inline-block size-[0.85em] object-cover"
 			/>
 			{part}
 		</span>
