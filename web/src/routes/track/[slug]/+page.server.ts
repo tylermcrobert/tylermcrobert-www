59f55e6f354fc1da3b1b6122147679f3ba4bd445
Tyler McRobert <tyler.mcrobert@gmail.com>
2026-04-30 17:01:40 -0700

    feat: wistful

--- web/src/routes/track/[slug]/+page.server.ts
+++ web/src/routes/track/[slug]/+page.server.ts
@@ -12,6 +12,19 @@ type SnippetsAPIResponse = {
 };
 
 export const load = async ({ params }) => {
+	// TODO: Remove this and surface it via api once integrating mux is complete
+	if (params.slug === 'wistful') {
+		return {
+			title:
+				'A Memory: A Moment / The Sky Above: A River, An Ember / A Borrowed Tune: Bless the Telephone',
+			primaryVersion: {
+				date: '2026-04-30',
+				duration: 0,
+				assetUrl: '/wistful.mp3'
+			}
+		};
+	}
+
 	const response = await fetch(
 		`https://snippets.tylermcrobert.com/api/tracks/${params.slug}`
 	);
